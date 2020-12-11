bb60aa1098f8d3636ba07115fe7cb3538e06d8b0
rcsmit <rcx.smit@gmail.com>
2020-12-11 22:20:04 +0100

    Update plot.py
    added horizontal lines for 'halveringen'

--- plot.py
+++ plot.py
@@ -11,8 +11,12 @@ import datetime as dt
 # startdate in m/d/yyyy
 
 START = 87875
-STARTDATE = "11/26/2020"
-NUMBEROFDAYS = 50
+# STARTDATE = "11/26/2020"
+
+
+STARTDATE = "10/26/2020"
+
+NUMBEROFDAYS = 100
 
 # R-numbers. Decrease and increase in two seperate figures
 Rz = [[0.75,0.8,0.85, 0.9,0.95,0.98,0.99], [1.01,1.05,1.1,1.2]]
@@ -41,15 +45,22 @@ for Rx in Rz:
     plt.xlabel('date')
     plt.ylabel('number of cases')
 
+    # add horizontal lines
+    plt.axhline(y=START/2, color='r', alpha=.4,linestyle='--')
+    plt.axhline(y=START/4, color='g', alpha=.4,linestyle='--')
+    plt.axhline(y=START/8, color='b', alpha=.4,linestyle='--')
+    plt.axhline(y=START/16, color='r', alpha=.4,linestyle='--')
+
+
     # Add a grid
     plt.grid(alpha=.4,linestyle='--')
 
     # Add a Legend
     plt.legend()
-
+    
     # lay-out of the x axis
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=10))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=5))
     plt.gcf().autofmt_xdate()
 
     # Show the plot
