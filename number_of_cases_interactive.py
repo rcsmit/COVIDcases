c2ccf06d7b3aaf753729895cf1bea310516ed431
rcsmit <rcx.smit@gmail.com>
2020-12-20 20:26:55 +0100

    Update number_of_cases_interactive.py

--- number_of_cases_interactive.py
+++ number_of_cases_interactive.py
@@ -141,10 +141,10 @@ plt.ylim(bottom = 0)
 #plt.ylim(0,450)
  
 # add horizontal lines and surfaces
-plt.fill_between(x, 0, 1250, color='#f392bd', alpha=0.3, label='waakzaam')
-plt.fill_between(x, 1251, 3750, color='#db5b94', alpha=0.3, label='zorgelijk')
-plt.fill_between(x, 3751, 6250, color='#bc2165', alpha=0.3, label='ernstig')
-plt.fill_between(x, 6250, 7500, color='#68032f', alpha=0.3, label='zeer ernstig')
+plt.fill_between(x, 0, 1250, color='#f392bd',  label='waakzaam')
+plt.fill_between(x, 1251, 3750, color='#db5b94',  label='zorgelijk')
+plt.fill_between(x, 3751, 6250, color='#bc2165',  label='ernstig')
+plt.fill_between(x, 6250, 10000, color='#68032f', label='zeer ernstig')
 #if Rnew>1:
 #    plt.fill_between(x, 500, 1000, color='grey', alpha=0.3, label='zeer zeer ernstig')
 
