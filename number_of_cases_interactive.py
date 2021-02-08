9dc95abe1f3b0c0b8dc2a88ee4b25fc04352d098
Rene Smit <rcx.smit@gmail.com>
2021-02-08 01:49:51 +0100

    Update number_of_cases_interactive.py

--- number_of_cases_interactive.py
+++ number_of_cases_interactive.py
@@ -296,11 +296,11 @@ for t in range(1, NUMBEROFDAYS):
         cummulative1.append   (cpt1)
         cummulative2.append   (cpt2 )
         cummulative12.append   (cpt12)
-    if (pt1+p2)>0:
+    if (pt1+pt2)>0:
         ratio_ =  ((pt2/(pt1+pt2)))
     else:
         ratio_ = 1
-        
+
     ratio.append   (100*ratio_)
     positivetestsper100k.append((pt1+pt2)/25)
 
