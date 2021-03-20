ae4325acb83445343e4f449715a6e6c3094a3db5
Rene Smit <rcx.smit@gmail.com>
2021-03-21 00:28:39 +0100

    Update postestennaarleeftyd.py

--- postestennaarleeftyd.py
+++ postestennaarleeftyd.py
@@ -1,53 +1,79 @@
+# ZIJN KINDEREN DE REDEN DAT HET PERCENTAGE POSITIEF DAALT ?
+#
+# René SMIT (@rcsmit) - MIT-licence
+# Geinspireerd door Maarten van den Berg (@mr_Smith_Econ)
+
+# IN: tabel met positief aantal testen en totaal aantal testen per week, gecategoriseerd naar leeftijd
+#     handmatig overgenomen uit Tabel 14 vh wekelijkse rapport van RIVM
+#     Wekelijkse update epidemiologische situatie COVID-19 in Nederland
+#     https://www.rivm.nl/coronavirus-covid-19/actueel/wekelijkse-update-epidemiologische-situatie-covid-19-in-nederland
+
+#     Ivm het simpel houden van de code enkele aanpassingen:
+#     0 -3 is toegevoegd bij 0 gevallen vóór de opsplitsing
+#     70+ => 70-97
+#     onbekend => 97-99
+
+# UIT : tabel   | weeknr    getest  positief  percentage  getest_weg  positief_weg  getest_metkids  positief_metkids  percentage_metkids|
+#       grafiek : https://twitter.com/rcsmit/status/1373308226624716806
+#       (één grafiek met de kinderen inbegrepen en één grafiek zonder kinderen inbegrepen)
+#       NB door een bugje ergens laat hij ook 2x een lege grafiek zien
+
+# TODO : functie maken voor het laten zien vd grafiek.
+#        lege grafiek weghalen
+
 import pandas as pd
 import matplotlib.pyplot as plt
-url = 'C:\\Users\\rcxsm\\Documents\\phyton_scripts\\covid19_seir_models\\input\\postestennaarleeftijd2.csv'
+
+url = 'https://raw.githubusercontent.com/rcsmit/COVIDcases/main/postestennaarleeftijd2.csv'
 df   = pd.read_csv(url,
                     delimiter=";",
                     low_memory=False)
-print (df)
+
 uitslag= pd.DataFrame({'weeknr': [],
                 'getest': [],'positief': [], 'percentage': [],
-                 'getest_metkids': [],'positief_metkids': [], 'percentage_metkids': []})
+                'getest_weg': [],'positief_weg': [],
+                'getest_metkids': [],'positief_metkids': [], 'percentage_metkids': []})
+
 actuele_week = 0
 
 for n in range (len(df)):
-
     huidige_week = df.loc[n]['weeknr']
     if huidige_week != actuele_week:
+
+        # uitslagen zonder kinderen
         cumm_getest = 0
         cumm_positief = 0
 
+        # niet getelde testen/uitslagen (WEGgelaten)
+        getest_weg = 0
+        positief_weg = 0
+
+        # uitslagen met kinderen (dus alles)
         cumm_getest_metkids = 0
         cumm_positief_metkids = 0
         actuele_week = df.loc[n]['weeknr']
 
     cumm_getest_metkids +=  (df.loc[n]['getest'])
     cumm_positief_metkids += (df.loc[n]['positief'])
-    if df.loc[n]['tot'] ==3 or  df.loc[n]['tot'] ==12:
-        pass
 
+    if df.loc[n]['tot'] ==3 or  df.loc[n]['tot'] ==12 or df.loc[n]['tot'] ==99:
+        getest_weg +=  (df.loc[n]['getest'])
+        positief_weg += (df.loc[n]['positief'])
     else:
-        #print (df.loc[n]['tot'])
-        #print (df.loc[n]['id'])
         cumm_getest +=  (df.loc[n]['getest'])
         cumm_positief += (df.loc[n]['positief'])
-        if df.loc[n]['tot'] ==99:
-            print (cumm_getest)
-            uitslag = uitslag.append({'weeknr':df.loc[n]['weeknr'],
-                'getest': cumm_getest,'positief': cumm_positief, 'percentage': round((cumm_positief/cumm_getest*100),1),
-                 'getest_metkids': cumm_getest_metkids,'positief_metkids': cumm_positief_metkids, 'percentage_metkids': round((cumm_positief_metkids/cumm_getest_metkids*100),1)  },ignore_index=True)
-            cumm_getest = 0
-            cumm_positief =0
+    if df.loc[n]['tot'] == 99:
+        uitslag = uitslag.append({'weeknr':df.loc[n]['weeknr'],
+            'getest': cumm_getest,'positief': cumm_positief, 'percentage': round((cumm_positief/cumm_getest*100),1),
+            'getest_weg': getest_weg,'positief_weg': positief_weg,
+            'getest_metkids': cumm_getest_metkids,'positief_metkids': cumm_positief_metkids, 'percentage_metkids': round((cumm_positief_metkids/cumm_getest_metkids*100),1)  },ignore_index=True)
+
 print (uitslag)
 
+# GRAFIEK ZONDER KINDEREN
+
 fig1x = plt.figure()
-ax = fig1x.add_subplot(111)
-plt.xlabel('week')
-
-# ax=uitslag["getest"].plot.bar(label="getest")
-# ax=uitslag["positief"].plot.bar(label="positief")
 ax = uitslag.plot(x="weeknr", y=["getest", "positief"], kind="bar")
-
 ax3=uitslag["percentage"].plot(secondary_y=True,linestyle='--', label="Percentage positive")
 
 ax.set_ylabel('aantal')
@@ -58,32 +84,24 @@ for ax in fig1x.axes:
             handles.append(h)
             labels.append(l)
 plt.legend(handles,labels)
-# Add a grid
 ax3.set_ylim(0,15)
 plt.title("Getest boven de 12 jaar" , fontsize=10)
 plt.show()
 
+# GRAFIEK MET KINDEREN
 
-
-fig1x = plt.figure()
-ax = fig1x.add_subplot(111)
-plt.xlabel('week')
-
-# ax=uitslag["getest"].plot.bar(label="getest")
-# ax=uitslag["positief"].plot.bar(label="positief")
+fig1y = plt.figure()
 ax = uitslag.plot(x="weeknr", y=["getest_metkids", "positief_metkids"], kind="bar")
-
 ax3=uitslag["percentage_metkids"].plot(secondary_y=True,linestyle='--', label="Percentage positive_metkids")
 
 ax.set_ylabel('aantal')
 ax3.set_ylabel('percentage')
 handles,labels = [],[]
-for ax in fig1x.axes:
+for ax in fig1y.axes:
         for h,l in zip(*ax.get_legend_handles_labels()):
             handles.append(h)
             labels.append(l)
 plt.legend(handles,labels)
-# Add a grid
 ax3.set_ylim(0,15)
 plt.title("Getest alles" , fontsize=10)
 plt.show()
