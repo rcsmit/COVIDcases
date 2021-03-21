76b3e5c258712492a87cb602bd83169d8ea070ab
Rene Smit <rcx.smit@gmail.com>
2021-03-21 17:17:35 +0100

    5 mln mensen immuun op dag 0

--- number_of_cases_interactive.py
+++ number_of_cases_interactive.py
@@ -68,7 +68,7 @@ showcummulative = st.sidebar.checkbox("Show cummulative / SIR", True)
 
 
 
-showimmunization = st.sidebar.checkbox("Immunization", True)
+showunization = st.sidebar.checkbox("Immunization", True)
 
 showSIR = st.sidebar.checkbox("Show classical SIR-model based on 100% second variant",True)
 #showSIR = False
@@ -84,7 +84,7 @@ if showcummulative or showSIR:
 
 
 if showcummulative or showSIR or showimmunization:
-    totalimmunedayzero_ = (st.sidebar.text_input('Total immune persons day zero', 2_500_000))
+    totalimmunedayzero_ = (st.sidebar.text_input('Total immune persons day zero', 5_000_000))
     totalpopulation_ = (st.sidebar.text_input('Total population', 17_500_000))
 
     testimmunefactor = st.sidebar.slider('Test/immunityfactor', 0.0, 5.0, 2.5)
