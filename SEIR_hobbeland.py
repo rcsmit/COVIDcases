4dc848d3a3479e55db110a1a3f1b23326689ff5e
Rene Smit <rcx.smit@gmail.com>
2021-04-20 23:52:24 +0200

    added vaccination

--- SEIR_hobbeland.py
+++ SEIR_hobbeland.py
@@ -1,14 +1,14 @@
-# Make an interactive version of the SEIR model, inspired by Hobbeland
+# Playing with the parameters of a classical SEIR models
+# René Smit, 20 February 2021, MIT-LICENSE
+# The parameters are taken from the story from Willemijn Coene about Hobbeland
 # https://twitter.com/MinaCoen/status/1362910764739231745
+# Alfa : 0.3333 / Beta : 1.25 / Gamma : 0.5 / R0 : 2.5
+# If there are strange results, just change the number of days a little bit. This is due to strange behavior of scipy's ODEINT. solve_ivp seems to be better
 
 
-
-# Sorry for all the commented out code, maybe I will combine the old and new version(s) later
-
-# Import our modules that we are using
 import math
 from datetime import datetime
-
+import copy
 import streamlit as st
 import numpy as np
 import matplotlib.dates as mdates
@@ -19,17 +19,10 @@ from matplotlib.font_manager import FontProperties
 _lock = RendererAgg.lock
 from scipy.integrate import odeint
 
-# VARIABLES
-# startdate in mm/dd/yyyy
-# variable1 = old variant
-# variable2 = new variant
-# variable 12 = old + new
-# variablex = with vaccination
 
 DATE_FORMAT = "%m/%d/%Y"
 b = datetime.today().strftime('%m/%d/%Y')
 
-#values 01/13/2021, according to https://www.bddataplan.nl/corona/
 st.sidebar.title('Parameters')
 
 numberofcasesdayz = (st.sidebar.text_input('Number infected persons on day zero (I0)', 100))
@@ -39,8 +32,8 @@ try:
 except:
     st.error("Please enter a number for the number of active cases on day zero")
     st.stop()
-    
-totalimmunedayzero = 0 
+
+totalimmunedayzero = 0
 
 st.markdown("<hr>", unsafe_allow_html=True)
 a = st.sidebar.text_input('startdate (mm/dd/yyyy)',"03/01/2021")
@@ -55,17 +48,17 @@ NUMBEROFDAYS = st.sidebar.slider('Number of days in graph', 15, 720, 100)
 global numberofdays_
 numberofdays_ = NUMBEROFDAYS
 
-Rstart = st.sidebar.slider('R-number R0', 0.1, 10.0, 2.5)
+Rstart = st.sidebar.slider('R-number first variant', 0.1, 10.0, 2.5)
 
 
 incubationtime = (st.sidebar.slider('Incubatietijd (1/alfa)', 1, 30, 3))
 
 infectioustime = (st.sidebar.slider('Average days infectious (1/gamma)', 1, 30, 2))
 
+start_day_vaccination = (st.sidebar.slider('Day on which the vaccination starts\n(set on max for no vaccination)', 1, NUMBEROFDAYS, int(NUMBEROFDAYS*0.2)))
+days_needed_for_vaccination = (st.sidebar.slider('Days needed for vaccination', 1, 3650, 365))
 
-
-
-totalpopulation_ = (st.sidebar.text_input('Total population (N)', 10_000_000))
+totalpopulation_ = (st.sidebar.text_input('Total population', 10_000_000))
 
 try:
     totalpopulation = int(totalpopulation_)
@@ -82,9 +75,6 @@ Tg_=Tg
 # Some manipulation of the x-values (the dates)
 then = startx + dt.timedelta(days=NUMBEROFDAYS)
 x = mdates.drange(startx,then,dt.timedelta(days=1))
-# x = dagnummer gerekend vanaf 1 januari 1970 (?)
-# y = aantal gevallen
-# z = dagnummer van 1 tot NUMBEROFDAYS
 z  = np.array(range(NUMBEROFDAYS))
 
 a_ = dt.datetime.strptime(a,'%m/%d/%Y').date()
@@ -102,17 +92,16 @@ infected = []
 ic  = []
 suspectible =[]
 recovered = []
-
+vaccinated = []
 suspectible.append(totalpopulation -totalimmunedayzero)
 #recovered.append(totalimmunedayzero )
 
 
 # START CALCULATING --------------------------------------------------------------------
 
-
 hospital.append(None)
 ic.append(None)
- 
+
 def th2r(rz):
     th = int( Tg_ * math.log(0.5) / math.log(rz))
     return th
@@ -134,9 +123,6 @@ def getsecondax():
     ax2.set_yticklabels(y2labels)
     ax2.set_ylim(*ax.get_ylim())
     ax2.set_ylabel('Halverings-/verdubbelingstijd (dagen)')
-
-
-
 
 def configgraph(titlex):
     interval_ = int(numberofdays_ / 20)
@@ -161,7 +147,7 @@ def configgraph(titlex):
 
 ################################################
 
-   
+
 # https://scipython.com/book/chapter-8-scipy/additional-examples/the-sir-epidemic-model/
 
 # Total population, N.
@@ -171,8 +157,8 @@ N = int(totalpopulation)
 
 # Initial number of infected and recovered individuals, I0 and R0.
 I0, R0 = int(numberofcasesdayz), totalimmunedayzero
-E0 = 0 
-
+E0 = 0
+V0 = 0
 
 # Everyone else, S0, is susceptible to infection initially.
 S0 = N - I0 -  R0 - E0
@@ -193,14 +179,15 @@ gamma = 1./infectioustime
 #beta = Rstart*gamma
 beta = Rstart*gamma/(S0/N)
 #beta, gamma = 0.2, 1./20
+zulu = 1/ days_needed_for_vaccination
 
 # reproductionrate = beta / gamma
 
-# β describes the effective contact rate of the disease: 
-# an infected individual comes into contact with βN other 
+# β describes the effective contact rate of the disease:
+# an infected individual comes into contact with βN other
 # individuals per unit time (of which the fraction that are
 # susceptible to contracting the disease is S/N).
-# 1/gamma is recovery rate in days 
+# 1/gamma is recovery rate in days
 
 # A grid of time points (in days)
 t = np.linspace(0, days, days)
@@ -208,28 +195,37 @@ t = np.linspace(0, days, days)
 # The SIR model differential equations.
 # https://scipython.com/book/chapter-8-scipy/additional-examples/the-sir-epidemic-model/
 def deriv(y, t, N, beta, gamma):
-    S, E, I, C, R = y
-    dSdt = -beta * S * I / N   # aantal zieke mensen x aantal gezonde mensen x beta
+    S, V, E, I, C, R = y
+    if V >= N:
+        dVdt = 0
+    else:
+        if t>start_day_vaccination:
+            zulu_= copy.deepcopy(zulu)
+            N_=copy.deepcopy(N)
+            dVdt = zulu_ * N_
+        else:
+            dVdt = 0
+    if S<=0:
+        dSdt = 0
+    else:
+        dSdt = (-beta * S * I / N)  - (dVdt) # aantal zieke mensen x aantal gezonde mensen x beta
+
     dEdt = beta * S * I / N  - alfa * E
     dIdt = alfa * E - gamma * I
     dCdt = alfa * E
     dRdt = gamma * I
-    
 
-    #print (dIdt)
-
-    # (n2/n1)^(Tg/t) 
-    return dSdt, dEdt, dIdt, dRdt, dCdt
+    return dSdt, dVdt, dEdt, dIdt, dRdt, dCdt
 
 # Initial conditions vector
-y0 = S0, E0, I0, C0, R0
+y0 = S0, V0, E0, I0, C0, R0
 # Integrate the SIR equations over the time grid, t.
 ret = odeint(deriv, y0, t, args=(N, beta, gamma))
-S, E, I, C, R  = ret.T
-
+S, V, E, I, C, R  = ret.T
 
 Tg = Tg_
 d = 1
+lambdaa = 1
 repr=[]
 repr.append(Rstart)
 repr_c=[]
@@ -237,24 +233,19 @@ repr_i=[]
 repr_c.append(None)
 repr_i.append(None)
 t = np.linspace(0, days, days)
-#print (I)
 Cnew=[]
-#Cnew.append(int(numberofcasesdayz))
 Cnew.append(None)
 for time in range(1,days):
     Cnew.append(C[time]-C[time-1])
-    
-    #if Cnew[time-1] == None:
-    if time == 1: 
+
+    if time == 1:
         repr_ = None
         repr_c_ = None
         repr_i_ = None
     else:
-        #repr_= (C[time]/C[time-1])**(Tg/d) 
-        repr_= (Cnew[time]/Cnew[time-1])**(Tg/d) 
-        repr_c_= (C[time]/C[time-1])**(Tg/d) 
-        repr_i_= (I[time]/I[time-1])**(Tg/d) 
-    #repr_= (I[time]/I[time-1])    
+        repr_= (Cnew[time]/Cnew[time-1])**(Tg/d)
+        repr_c_= (C[time]/C[time-1])**(Tg/d)
+        repr_i_= (I[time]/I[time-1])**(Tg/d)
     repr.append(repr_)
     repr_c.append(repr_c_)
     repr_i.append(repr_i_)
@@ -265,14 +256,15 @@ disclaimerSIR= ('<div class=\"infobox\"><h1>Classical SEIR-graphs</h1>'
                     '<a href=\"https://twitter.com/MinaCoen/status/1362910764739231745\" target=\"_blank\">'
                     ' the story from Willemijn Coene about Hobbeland</a></p>'
 
-                    
-                     '<p> See also <a href=\"https://share.streamlit.io/rcsmit/covidcases/main/number_of_cases_interactive.py\"'
+
+                     '<p> See <a href=\"https://share.streamlit.io/rcsmit/covidcases/main/number_of_cases_interactive.py\"'
                     ' target=\"_blank\">'
-                    'my illustrative and simple model about the Netherlands</a>. </p>'
+                    'for my illustrative and simple model about the Netherlands</a>. </p>'
 
 
-                    
+
                     '<p>Alfa : ' + str(round(alfa,4)) + ' / Beta : ' + str(round(beta,4)) + ' / Gamma : ' + str(gamma) + ' / R<sub>0</sub> : '+ str(Rstart) + '</p>'
+                    '<p>If there are strange results, just change the number of days/parameters a little bit. This is due to strange behavior of scipy\'s ODEINT. solve_ivp seems to be better</p>'
                     '</div>'
     )
 
@@ -280,15 +272,14 @@ st.markdown(disclaimerSIR, unsafe_allow_html=True)
 
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig2a = plt.figure(facecolor='w')
-#ax = fig2a.add_subplot(111, facecolor='#dddddd', axisbelow=True)
 ax = fig2a.add_subplot(111, axisbelow=True)
-ax.plot(x, S, 'y', alpha=0.5, lw=2, label='Susceptible')
-ax.plot(x, E, 'g', alpha=0.5, lw=2, label='Exposed')
+ax.plot(x, S, 'b', alpha=0.5, lw=2, label='Susceptible')
+ax.plot(x, E, 'purple', alpha=0.5, lw=2, label='Exposed')
+ax.plot(x, V, 'yellow', alpha=0.5, lw=2, label='Vaccinated')
 
-ax.plot(x, I, 'r', alpha=0.5, lw=2, label='Infected / Ziek')
-#ax.plot(x, Cnew, 'blue', alpha=0.5, lw=2, label='New Cases')
-ax.plot(x, R, 'purple', alpha=0.5, lw=2, label='Recovered with immunity')
-#ax.plot(t, repr, 'yellow', alpha=0.5, lw=2, label='R getal')
+ax.plot(x, I, 'r', alpha=0.5, lw=2, label='Infected')
+ax.plot(x, Cnew, 'orange', alpha=0.5, lw=2, label='New Cases')
+ax.plot(x, R, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
 ax.set_xlabel('Time (days)')
 ax.set_ylabel('Number')
 ax.yaxis.set_tick_params(length=0)
@@ -299,15 +290,16 @@ configgraph(titlex)
 plt.show()
 st.pyplot(fig2a)
 
+st.write  ("attack rate classical SIR model : " + str(int(C[days-1])) + " mensen / "+ str(round(100*((C[days-1])        /N),2))+ " %")
+st.markdown ("Theoretical herd immunity treshhold (HIT) (1 - [1/"+str(Rstart)+"]<sup>1/"+ str(lambdaa)+ "</sup>) : " + str(round(100*(1-((1/Rstart)**(1/lambdaa))),2))+ " % = " + str(round(N*(1-((1/Rstart)**(1/lambdaa))),0))+ " persons", unsafe_allow_html=True)
+st.write ("Attack rate = final size of the epidemic (FSE) ")
+
 
 # New cases
 fig2c = plt.figure(facecolor='w')
 ax = fig2c.add_subplot(111,  axisbelow=True)
-#ax.plot(t, C, 'green', alpha=0.5, lw=2, label='Cases')
 ax.plot(x, Cnew, 'blue',  label='New Cases')
 ax.plot(x, I, 'red', alpha=0.5,  label='Infected')
-
-#ax.plot(t, repr, 'yellow', alpha=0.5, lw=2, label='R getal')
 ax.set_xlabel('Time (days)')
 ax.set_ylabel('Number')
 ax.yaxis.set_tick_params(length=0)
@@ -321,20 +313,15 @@ st.pyplot(fig2c)
 fig2b = plt.figure(facecolor='w')
 ax = fig2b.add_subplot(111,  axisbelow=True)
 ax.plot(x, repr, 'b', alpha=0.5, lw=2, label='R getal_ based on Cnew')
-#ax.plot(t, repr_i, 'b', alpha=0.5, lw=2, label='R getal based on I')
-#ax.plot(t, repr_c, 'g', alpha=0.5, lw=2, label='R getal based on C')
 ax.set_xlabel('Time (days)')
 ax.set_ylabel('R getal')
-#ax.set_ylim(0,2)
 ax.axhline(y=1, color='yellow', alpha=.6,linestyle='--')
-# ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-# legend = ax.legend()
 titlex = "Gliding R-number"
 configgraph(titlex)
 plt.show()
 st.pyplot(fig2b)
-lambdaa = 1
-st.write  ("attack rate classical SIR model : " + str(round(100*((C[days-1])        /N),2))+ " %")
+
+st.write  ("attack rate classical SIR model : " + str(int(C[days-1])) + " mensen / "+ str(round(100*((C[days-1])        /N),2))+ " %")
 st.markdown ("Theoretical herd immunity treshhold (HIT) (1 - [1/"+str(Rstart)+"]<sup>1/"+ str(lambdaa)+ "</sup>) : " + str(round(100*(1-((1/Rstart)**(1/lambdaa))),2))+ " % = " + str(round(N*(1-((1/Rstart)**(1/lambdaa))),0))+ " persons", unsafe_allow_html=True)
 st.write ("Attack rate = final size of the epidemic (FSE) ")
 repr=[]
@@ -345,34 +332,12 @@ tekst = (
     '<style> .infobox {  background-color: lightblue; padding: 5px;}</style>'
     '<hr><div class=\'infobox\'>Made by Rene Smit. (<a href=\'http://www.twitter.com/rcsmit\' target=\"_blank\">@rcsmit</a>) <br>'
     'Overdrachtstijd is 4 dagen. Disclaimer is following. Provided As-is etc.<br>'
-    'Sourcecode : <a href=\"https://github.com/rcsmit/COVIDcases/blob/main/SEIR_hobbeland.py\" target=\"_blank\">github.com/rcsmit</a><br>'
-    'How-to tutorial : <a href=\"https://rcsmit.medium.com/making-interactive-webbased-graphs-with-python-and-streamlit-a9fecf58dd4d\" target=\"_blank\">rcsmit.medium.com</a><br>')
+    'Sourcecode : <a href=\"https://github.com/rcsmit/COVIDcases/edit/main/number_of_cases_interactive.py\" target=\"_blank\">github.com/rcsmit</a><br>'
+    'How-to tutorial : <a href=\"https://rcsmit.medium.com/making-interactive-webbased-graphs-with-python-and-streamlit-a9fecf58dd4d\" target=\"_blank\">rcsmit.medium.com</a><br>'
+    'Inspired by <a href=\"https://twitter.com/mzelst/status/1350923275296251904\" target=\"_blank\">this tweet</a> of Marino van Zelst.<br>'
+    'With help of <a href=\"https://twitter.com/hk_nien" target=\"_blank\">Han-Kwang Nienhuys</a>.</div>')
 
-links = (
-'<h3>Useful dashboards</h3><ul>'
-'<li><a href=\"https://allecijfers.nl/nieuws/statistieken-over-het-corona-virus-en-covid19/\" target=\"_blank\">Allecijfers.nl</a></li>'
-'<li><a href=\"https://datagraver.com/corona\" target=\"_blank\">https://www.datagraver/corona/</a></li>'
-'<li><a href=\"https://www.bddataplan.nl/corona\" target=\"_blank\">https://www.bddataplan.nl/corona/</a></li>'
-'<li><a href=\"https://renkulab.shinyapps.io/COVID-19-Epidemic-Forecasting/_w_ebc33de6/_w_dce98783/_w_0603a728/_w_5b59f69e/?tab=jhu_pred&country=France\" target=\"_blank\">Dashboard by  Institute of Global Health, Geneve, Swiss</a></li>'
-'<li><a href=\"https://coronadashboard.rijksoverheid.nl/\" target=\"_blank\">Rijksoverheid NL</a></li>'
-'<li><a href=\"https://www.corona-lokaal.nl/locatie/Nederland\" target=\"_blank\">Corona lokaal</a></li>'
-'</ul>'
-
-
-'<h3>Other (SEIR) models</h3><ul>'
-'<li><a href=\"http://gabgoh.github.io/COVID/index.html\" target=\"_blank\">Epidemic Calculator </a></li>'
-'<li><a href=\"https://www.covidsim.org" target=\"_blank\">COVID-19 Scenario Analysis Tool (Imperial College London)</a></li>'
-'<li><a href=\"https://covid19-scenarios.org/\" target=\"_blank\">Covid scenarios</a></li>'
-'<li><a href=\"https://share.streamlit.io/lcalmbach/pandemic-simulator/main/app.py\" target=\"_blank\">Pandemic simulator</a></li>'
-'<li><a href=\"https://penn-chime.phl.io/\" target=\"_blank\">Hospital impact model</a></li>'
-'<li><a href=\"http://www.modelinginfectiousdiseases.org/\" target=\"_blank\">Code from the book Modeling Infectious Diseases in Humans and Animals '
-'(Matt J. Keeling & Pejman Rohani)</a></li></ul>'
-'<h3>Other sources/info</h3>'
-'<ul><li><a href=\"https://archive.is/dqOjs\" target=\"_blank\">Waarom bierviltjesberekeningen over het virus niet werken</a></li>'
-'<li><a href=\"https://www.scienceguide.nl/2020/03/modellen-geven-geen-absolute-zekerheid-maar-ze-zijn-ontontbeerlijk/\" target=\"_blank\">Modellen geven geen absolute zekerheid, maar ze zijn onontbeerlijk</a></li>'
-'<li><a href=\"https://www.nature.com/articles/d41586-020-02009-ws\" target=\"_blank\">A guide to R — the pandemic’s misunderstood metric</a></li></ul>')
 
 
 st.sidebar.markdown(tekst, unsafe_allow_html=True)
-#st.sidebar.info(tekst)
 st.markdown(links, unsafe_allow_html=True)
