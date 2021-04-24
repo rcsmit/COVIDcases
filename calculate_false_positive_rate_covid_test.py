837acf61fa3d7470bf832962c8366b96b8805cbb
Rene Smit <rcx.smit@gmail.com>
2021-04-25 00:46:03 +0200

    Update calculate_false_positive_rate_covid_test.py

--- calculate_false_positive_rate_covid_test.py
+++ calculate_false_positive_rate_covid_test.py
@@ -1,4 +1,4 @@
-# CALCULATE FALSE POSITIVITY RATE AT VARIOUS LEVELS OF INFECTED PEOPLE ('besmettelijken')
+# CALCULATE FALSE POSITIVITY RATE AT VARIOUS LEVELS OF CONTAGIOUS PEOPLE ('besmettelijken')
 # René Smit, 14 april 2021, MIT LICENSE
 
 from tabulate import tabulate
@@ -20,13 +20,13 @@ def calculate(test, prevalentie, number_of_tested_people):
     false_positive = round((total_healthy * (1 - specificity)),0)
     data = [
         [
-            "Result: healthy (-)",
+            "Result: 'healthy' (-)",
             true_negative,
             false_negative,
             true_negative + false_negative,
         ],
         [
-            "Result: sick (+) ",
+            "Result: 'sick' (+) ",
             false_positive,
             true_positive,
             false_positive + true_positive,
@@ -41,23 +41,23 @@ def calculate(test, prevalentie, number_of_tested_people):
     fpr = round(100*false_positive/(false_positive + true_positive),1)
     fnr = round(100*false_negative/(false_negative + true_negative),3)
     pos = 100*(true_positive + false_positive)/number_of_tested_people
-    output = False
-    if output == True:
+    output = True
+    if output:
         print("--------------------------------------------------------------------------")
         print()
         print (f"Prevalentie = {round(prevalentie*100,2)} % ({prevalentie*17500000}/17.5 miljoen)\nNumber of tested people : {number_of_tested_people}")
         print()
 
-        print(f"Name test: {name} - sensitivity : {test[1]} - specificity : {test[2]}")
+        print(f"Name test: {name} - specificity : {test[2]} - sensitivity : {test[1]}\n")
 
-        print(tabulate(data, headers=["", "Healthy (-)\nSpecificity", "Sick (+)\nSensitivity", "Total"]))
+        print(tabulate(data, headers=["", "'Healthy' (-)\nSpecificity", "'Sick' (+)\nSensitivity", "Total"]))
 
         print()
         print(
-            f"Positive predictive value (PPV): {100-fpr} % - (Tested sick while you are sick)"
+            f"Positive predictive value (PPV): {round(100-fpr,3)} % - (Tested sick while you are sick)"
         )
         print(
-            f"Negative predictive value (NPV): {100-fnr} % - (Tested sick while you are sick)"
+            f"Negative predictive value (NPV): {round(100-fnr,3)} % - (Tested sick while you are sick)"
         )
         print()
         print(
@@ -80,6 +80,8 @@ def main():
     #  ["PCR TEST", 0.95, 0.998], ["PCR TEST WHO", 0.95, 0.97]]
 
     testen_ = [["ROCHE SELFTEST", 0.8, 0.97], ["PCR TEST", 0.95, 0.998]]
+    #testen_ = [["538 TEST", 0.8, 0.999], ["PCR TEST", 0.95, 0.998]]
+    #testen_ = [["538 TEST", 0.8, 0.7], ["PCR TEST", 0.95, 0.96]]
 
     for testen in testen_:
         titel = (f"{testen[0]} - sensitivity {testen[1]} - specificity {testen[2]}")
@@ -88,40 +90,45 @@ def main():
         false_negative_rate = []
         chance_to_be_tested_positive = []
 
-        for b in range (5_000, 200_000, 20_000):
-        # b = 50_000 # aantal besmettelijken
-        # if b != None:
+        population = 17_500_000
+
+        # (un)comment next one line to have a loop of "contagious people"
+        #for b in range (5_000, 200_000, 20_000):
+
+        # (un)comment next two lines to have 1 value of 'contagious people'
+        b = 5_000 # aantal besmettelijken
+        
+        if b != None:
             number_of_tested_people = 10000.0  # don't make too small to prevent rouding errors
 
-            prevalentie = b / 17_500_000
+            prevalentie = b / population
             fpr, fnr, pos = calculate(testen, prevalentie, number_of_tested_people)
             besm.append(b)
             false_positive_rate.append(fpr)
             false_negative_rate.append(fnr)
             chance_to_be_tested_positive.append(pos)
 
-        graph = True
-        if graph == True:
+        graph = False
+        if graph :
             fig = plt.figure()
             ax = fig.add_subplot(111)
             ax3 = ax.twinx()
             plt.title(titel)
 
-            # SHOW FALSE POS AND FALSE NEG RATE
+            ax.set_xlabel('aantal besmettelijken in NL (#)')
+
+            # (un)comment next lines (not) to   SHOW FALSE POS AND FALSE NEG RATE
             # ax.plot(besm,false_positive_rate,  'r',marker='o',)
             # ax3.plot(besm,false_negative_rate,'g',  marker='o',)
             # ax.set_ylabel('red: false positive rate (%)')
             # ax3.set_ylabel('green: false negative rate (%)')
 
-            # SHOW CHANCE TO BE TESTED POSITIVE and FALSE POS RATE
+            # (un)comment next lines (not) to  SHOW CHANCE TO BE TESTED POSITIVE and FALSE POS RATE
             ax.plot(besm,chance_to_be_tested_positive,'g',  marker='o',)
             ax3.plot(besm,false_positive_rate,  'r',marker='o',)
             ax.set_ylabel('green: chance to be tested positive (%)')
             ax3.set_ylabel('red: false positive rate (%)')
-
             
-
-            ax.set_xlabel('aantal besmettelijken in NL (#)')
             plt.show()
 
 if __name__ == "__main__":
@@ -131,21 +138,22 @@ if __name__ == "__main__":
 """
 https://www.healthnewsreview.org/toolkit/tips-for-understanding-studies/understanding-medical-tests-sensitivity-specificity-and-positive-predictive-value/
 Sensitivity measures how often a test correctly generates a positive result
- for people who have the condition that’s being tested for
- (also known as the “true positive” rate). A test that’s highly sensitive
- will flag almost everyone who has the disease and not generate many
- false-negative results. (Example: a test with 90% sensitivity will
- correctly return a positive result for 90% of people who have the disease,
-  but will return a negative result — a false-negative — for 10% of the people who
-   have the disease and should have tested positive.)
+for people who have the condition that’s being tested for
+(also known as the “true positive” rate). A test that’s highly sensitive
+will flag almost everyone who has the disease and not generate many
+false-negative results. (Example: a test with 90% sensitivity will
+correctly return a positive result for 90% of people who have the disease,
+but will return a negative result — a false-negative — for 10% of the people who
+have the disease and should have tested positive.)
 
 Specificity measures a test’s ability to correctly generate a
 negative result for people who don’t have the condition that’s
- being tested for (also known as the “true negative” rate).
- A high-specificity test will correctly rule out almost everyone
-  who doesn’t have the disease and won’t generate many
-  false-positive results. (Example: a test with 90% specificity will
-  correctly return a negative result for 90% of people who don’t have the disease, but will return a positive result — a false-positive — for 10% of the people who don’t have the disease and should have tested negative.)
+being tested for (also known as the “true negative” rate).
+A high-specificity test will correctly rule out almost everyone
+who doesn’t have the disease and won’t generate many
+false-positive results. (Example: a test with 90% specificity will
+correctly return a negative result for 90% of people who don’t have the disease, but will return a positive result — a false-positive — for 10% of the people who don’t have the disease and should have tested negative.)
+
 SEE ALSO :
 # https://lci.rivm.nl/covid-19/bijlage/aanvullend
 """
