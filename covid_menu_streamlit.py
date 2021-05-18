8487edd95e8eebbbbf278dc5653d6f3becf30f81
Rene Smit <rcx.smit@gmail.com>
2021-05-19 00:08:38 +0200

    Update covid_menu_streamlit.py

--- covid_menu_streamlit.py
+++ covid_menu_streamlit.py
@@ -13,14 +13,14 @@ import streamlit as st
 
 def main():
     lijst = ["welcome", "fit_to_data_streamlit", "covid_dashboard_rcsmit", "plot_hosp_ic_streamlit", "number_of_cases_interactive", "SEIR_hobbeland",
-     "grafiek_pos_testen_per_leeftijdscategorie_streamlit", "perprovincieperleeftijd", "calculate_ifr_from_prevalence_streamlit"]
+     "grafiek_pos_testen_per_leeftijdscategorie_streamlit", "perprovincieperleeftijd", "calculate_ifr_from_prevalence_streamlit", "calculate_false_positive_rate_covid_test_streamlit"]
 
     what_to_do =  st.sidebar.selectbox(
             "What to dp", lijst,
             index=0,
         )
 
-    if what_to_do == "welcome":
+    if if what_to_do == "welcome":
         welcome.main()
 
     elif what_to_do == "fit_to_data_streamlit":
@@ -39,7 +39,8 @@ def main():
         number_of_cases_interactive.main()
     elif what_to_do == "calculate_ifr_from_prevalence_streamlit":
         calculate_ifr_from_prevalence_streamlit.main()
-
+    elif what_to_do == "calculate_false_positive_rate_covid_test_streamlit":
+        calculate_false_positive_rate_covid_test_streamlit.main()
 
 if __name__ == "__main__":
     main()
