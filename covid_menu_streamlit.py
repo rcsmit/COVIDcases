9804d14a9cc7c4c3628e91fcbb637a7714d23b6f
Rene Smit <rcx.smit@gmail.com>
2021-05-19 00:15:27 +0200

    Update covid_menu_streamlit.py

--- covid_menu_streamlit.py
+++ covid_menu_streamlit.py
@@ -1,12 +1,12 @@
-import fit_to_data_streamlit
-import covid_dashboard_rcsmit
-import plot_hosp_ic_streamlit
-import number_of_cases_interactive
-import SEIR_hobbeland
-import grafiek_pos_testen_per_leeftijdscategorie_streamlit
-import perprovincieperleeftijd
+
+
+
+
+
+
+
 import welcome
-import calculate_ifr_from_prevalence_streamlit
+
 
 
 import streamlit as st
@@ -21,25 +21,36 @@ def main():
         )
 
     if  what_to_do == "welcome":
+        import welcome
         welcome.main()
 
     elif what_to_do == "fit_to_data_streamlit":
+        import fit_to_data_streamlit
         fit_to_data_streamlit.main()
+        import covid_dashboard_rcsmit
     elif  what_to_do == "covid_dashboard_rcsmit" :
+        import covid_dashboard_rcsmit
         covid_dashboard_rcsmit.main()
     elif  what_to_do == "plot_hosp_ic_streamlit":
+        import plot_hosp_ic_streamlit
         plot_hosp_ic_streamlit.main()
     elif what_to_do == "SEIR_hobbeland":
+        import SEIR_hobbeland
         SEIR_hobbeland.main()
     elif what_to_do == "grafiek_pos_testen_per_leeftijdscategorie_streamlit":
+        import grafiek_pos_testen_per_leeftijdscategorie_streamlit
         grafiek_pos_testen_per_leeftijdscategorie_streamlit.main()
     elif what_to_do == "perprovincieperleeftijd":
+        import perprovincieperleeftijd
         perprovincieperleeftijd.main_per_province_per_leeftijd()
     elif what_to_do == "number_of_cases_interactive":
+        import number_of_cases_interactive
         number_of_cases_interactive.main()
     elif what_to_do == "calculate_ifr_from_prevalence_streamlit":
+        import calculate_ifr_from_prevalence_streamlit
         calculate_ifr_from_prevalence_streamlit.main()
     elif what_to_do == "calculate_false_positive_rate_covid_test_streamlit":
+        import calculate_false_positive_rate_covid_test_streamlit
         calculate_false_positive_rate_covid_test_streamlit.main()
 
 if __name__ == "__main__":
