4865b4147639a3af9cd12f75b77b6f06efd75347
Rene Smit <rcx.smit@gmail.com>
2021-05-18 23:14:21 +0200

    Update covid_menu_streamlit.py

--- covid_menu_streamlit.py
+++ covid_menu_streamlit.py
@@ -1,23 +1,28 @@
 import fit_to_data_streamlit
 import covid_dashboard_rcsmit
 import plot_hosp_ic_streamlit
+import number_of_cases_interactive
 import SEIR_hobbeland
 import grafiek_pos_testen_per_leeftijdscategorie_streamlit
 import perprovincieperleeftijd
+import welcome
+
 
 import streamlit as st
 
 def main():
-    lijst = ["fit_to_data_streamlit", "covid_dashboard_rcsmit", "plot_hosp_ic_streamlit", "SEIR_hobbeland",
+    lijst = ["welcome", "fit_to_data_streamlit", "covid_dashboard_rcsmit", "plot_hosp_ic_streamlit", "number_of_cases_interactive", "SEIR_hobbeland",
      "grafiek_pos_testen_per_leeftijdscategorie_streamlit", "perprovincieperleeftijd"]
 
     what_to_do =  st.sidebar.selectbox(
             "What to dp", lijst,
-            index=3,
+            index=0,
         )
 
+    if if what_to_do == "welcome":
+        welcome.main()
 
-    if what_to_do == "fit_to_data_streamlit":
+    elif what_to_do == "fit_to_data_streamlit":
         fit_to_data_streamlit.main()
     elif  what_to_do == "covid_dashboard_rcsmit" :
         covid_dashboard_rcsmit.main()
@@ -29,8 +34,9 @@ def main():
         grafiek_pos_testen_per_leeftijdscategorie_streamlit.main()
     elif what_to_do == "perprovincieperleeftijd":
         perprovincieperleeftijd.main_per_province_per_leeftijd()
+    elif what_to_do == "number_of_cases_interactive":
+        number_of_cases_interactive.main()
 
-    st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
