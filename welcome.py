f5134b8e9035a98f60e29fcf0d4f03b0c179ac04
Rene Smit <rcx.smit@gmail.com>
2021-05-18 23:13:07 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -3,6 +3,33 @@ import streamlit as st
 
 def main():
     st.header ("Welcome!")
+    # "welcome", "fit_to_data_streamlit", "covid_dashboard_rcsmit", "plot_hosp_ic_streamlit", "number_of_cases_interactive", "SEIR_hobbeland",
+     "grafiek_pos_testen_per_leeftijdscategorie_streamlit", "perprovincieperleeftijd"
+        number_of_cases_interactive.py - Plotting the number of COVID cases with different values. Contains a SIR-graph and a classical SIR-model. Including immunity
+
+
+# covid_dashboard_rcsmit.py - aggregates a lot of information and statistics from the Netherlands. Shows correlations and graphs. image
+# grafiek_pos_testen_per_leeftijdscategorie_streamlit.py - draw graphs of positieve cases per age in time image
+# SEIR_hobbeland.py - Make an interactive version of the SEIR model, inspired by Hobbeland - https://twitter.com/MinaCoen/status/1362910764739231745 image
+
+# calculate_false_positive_rate_covid_test.py - HOE BETROUWBAAR ZIJN DE TESTEN ? image image
+
+# fit_to_data_streamlit.py - FIT THE DATA image
+
+# calculate_ifr_prevalence.py - calculate percentage of population who had covid and the IFR from the prevalence
+
+
+# stackplot.py - draw a stackplot of portion of agegroups in time for positive tests, hospitalizations and deceased from cases_landelijk.csv
+
+# stackplot_hosp_ic.py - draw a stackplot of portion of agegroups in time (weekly data) for hospitalizations and ICU admission from ziekenhuis_ic_opnames_per_leeftijdsgroep.csv
+
+# image image
+
+# postestennaarleeftyd.py - ZIJN KINDEREN DE REDEN DAT HET PERCENTAGE POSITIEF DAALT ? image
+
+# getest_leeftijd_weekcijfers.py - IS ER EEN VERBAND MET HET PERCENTAGE POSITIEF PER LEEFTIJDSGROEP EN DE ZIEKENHUISOPNAMES? image
+
+
 
 if __name__ == "__main__":
     main()
