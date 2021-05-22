ed0e9f0e2b857f64fc1ddd63c106adaa312512c4
Rene Smit <rcx.smit@gmail.com>
2021-05-22 13:40:42 +0200

    Update covid_dashboard_rcsmit.py

--- covid_dashboard_rcsmit.py
+++ covid_dashboard_rcsmit.py
@@ -696,9 +696,9 @@ def last_manipulations(df, what_to_drop, drop_last):
         df = df[:drop_last]  # drop last row(s)
 
     df.insert(df.shape[1], "q_biggerthansix", None)
-    
+
     df.insert(df.shape[1], "q_smallerthansix", None)
-    
+
     return df, werkdagen, weekend_
 
 
@@ -1648,7 +1648,7 @@ def main():
         "deceased_per_prev_div_days_contagious",
         "Deceased_cumm_div_prev_div_days_contagious_cumm",
         "Deceased_cumm_period_div_prev_div_days_contagious_cumm_period",
-     
+
         "reported_corrected",
         "onderrapportagefactor"
     ]
@@ -1715,12 +1715,18 @@ def main():
 
 
 
+    #st.write(get_duplicate_cols(df))
+    df, newcolumns_w2w, newcolumns2_w2w = week_to_week(df, w2w)
+
+    lijst.extend(newcolumns_w2w) # percentage
+    lijst.extend(newcolumns2_w2w) # index
+
+    df, newcolumns_w2w2, newcolumns2_w2w2 = week_to_week(df, newcolumns)
+    lijst.extend(newcolumns_w2w2) # percentage
+
     chd = ["pos_test_0-9", "pos_test_10-19", "pos_test_20-29", "pos_test_30-39", "pos_test_40-49", "pos_test_50-59", "pos_test_60-69", "pos_test_70-79", "pos_test_80-89", "pos_test_90+", "pos_test_20-99","pos_test_0-99", "hosp_0-9", "hosp_10-19", "hosp_20-29", "hosp_30-39", "hosp_40-49", "hosp_50-59", "hosp_60-69", "hosp_70-79", "hosp_80-89", "hosp_90+", "hosp_0-49","hosp_50-79","hosp_70+", "hosp_0-90", "deceased_<50", "deceased_50-59", "deceased_60-69", "deceased_70-79", "deceased_80-89", "deceased_90+", "deceased_0-99"]
     mzelst = ["date","cases","hospitalization","deaths","positivetests","hospital_intake_rivm","Hospital_Intake_Proven","Hospital_Intake_Suspected","IC_Intake_Proven","IC_Intake_Suspected","IC_Current","ICs_Used","IC_Cumulative","Hospital_Currently","IC_Deaths_Cumulative","IC_Discharge_Cumulative","IC_Discharge_InHospital","Hospital_Cumulative","Hospital_Intake","IC_Intake","Hosp_Intake_Suspec_Cumul","IC_Intake_Suspected_Cumul","IC_Intake_Proven_Cumsum","IC_Bedden_COVID","IC_Bedden_Non_COVID","Kliniek_Bedden","IC_Nieuwe_Opnames_COVID","Kliniek_Nieuwe_Opnames_COVID","Totaal_bezetting","IC_Opnames_7d","Kliniek_Opnames_7d","Totaal_opnames","Totaal_opnames_7d","Totaal_IC","IC_opnames_14d","Kliniek_opnames_14d","OMT_Check_IC","OMT_Check_Kliniek","new.infection","corrections.cases","net.infection","new.hospitals","corrections.hospitals","net.hospitals","new.deaths","corrections.deaths","net.deaths","positive_7daverage","infections.today.nursery","infections.total.nursery","deaths.today.nursery","deaths.total.nursery","mutations.locations.nursery","total.current.locations.nursery","values.tested_total","values.infected","values.infected_percentage","pos.rate.3d.avg"]
-    #st.write(get_duplicate_cols(df))
-    df, newcolumns, newcolumns2 = week_to_week(df, w2w)
-    lijst.extend(newcolumns)
-    lijst.extend(newcolumns2)
+
     lijst.extend(chd)
     lijst.extend(mzelst)
 
