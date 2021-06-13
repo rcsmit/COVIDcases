3129c50e1bdc2e05ead25e42ad8a303560d2e188
Rene Smit <rcx.smit@gmail.com>
2021-06-13 23:28:12 +0200

    .

--- covid_menu_streamlit.py
+++ covid_menu_streamlit.py
@@ -27,7 +27,7 @@ def main():
             ["9. r getal per provincie","r getal per provincie"],
             ["10. Cases from suspectibles", "cases_from_susp_streamlit"],
             ["11. Fit to data OWID", "fit_to_data_owid_streamlit_animated"],
-            ["12. Calculate R per country owid", "calculate_r_per_country_owid_streamlit"]
+            ["12. Calculate R per country owid", "calculate_r_per_country_owid_streamlit"],
             ["13. grafiek pos testen per leeftijdscat","grafiek pos testen per leeftijdscategorie streamlit"],
             ["14. per provincie per leeftijd","perprovincieperleeftijd"]]
 
