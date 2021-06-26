0ed0d366be2b09ddd9cad6a62e28163e794185b8
Rene Smit <rcx.smit@gmail.com>
2021-06-26 16:55:57 +0200

    .

--- helpers.py
+++ helpers.py
@@ -91,14 +91,14 @@ def make_scatterplot(df_temp, what_to_show_l, what_to_show_r, FROM, UNTIL,  show
 
                         plt.scatter(x__, y__,  s=2,color=c)
             else:
-            x_ = np.array(df_temp[what_to_show_l])
-            y_ = np.array(df_temp[what_to_show_r])
+                x_ = np.array(df_temp[what_to_show_l])
+                y_ = np.array(df_temp[what_to_show_r])
 
 
-            plt.scatter(x_, y_)
+                plt.scatter(x_, y_)
 
-            x_ = np.array(df_temp[what_to_show_l])
-            y_ = np.array(df_temp[what_to_show_r])
+                x_ = np.array(df_temp[what_to_show_l])
+                y_ = np.array(df_temp[what_to_show_r])
 
 
 
