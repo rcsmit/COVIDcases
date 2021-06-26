6c1685aa946c23e1b233bfa297ef6a0c87c966bb
Rene Smit <rcx.smit@gmail.com>
2021-06-26 18:09:33 +0200

    .

--- helpers.py
+++ helpers.py
@@ -145,7 +145,7 @@ def make_scatterplot(df_temp, what_to_show_l, what_to_show_r, FROM, UNTIL,  show
 
                         plt.scatter(x__, y__,  s=2,color=c)
                         st.write(c)
-                        r,g,b = c
+                        r,g,b,z = c
                         st.write (f"{m} {y} - {RGBtoHexConverion(r,g,b)}")
             else:
                 x_ = np.array(df_temp[what_to_show_l])
