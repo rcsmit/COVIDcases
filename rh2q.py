542ae00df341fee6603c8eaf5d67e1786b47e681
Rene Smit <rcx.smit@gmail.com>
2021-06-27 14:16:28 +0200

    .

--- rh2q.py
+++ rh2q.py
@@ -1,40 +1,44 @@
 import streamlit as st
 import math
 
-def rh2q(rh, t, p ):
+
+def rh2q(rh, t, p):
+
+    # https://github.com/PecanProject/pecan/blob/master/modules/data.atmosphere/R/metutils.R#L45
+    es = 6.112 * math.exp((17.67 * t) / (t + 243.5)) # Saturation Vapor Pressure
+    e = es * (rh / 100) # vapor pressure
+    q_ = (0.622 * e) / (p - (0.378 * e)) * 1000
+    # In het dashboard: minimale relatieve luchtvochtigheid en de maximale temperatuur (per dag) van de 24 uurs waardes van De Bilt door het KNMI
+
+    # More info:
     # https://archive.eol.ucar.edu/projects/ceop/dm/documents/refdata_report/eqns.html
+    # https://cran.r-project.org/web/packages/humidity/vignettes/humidity-measures.html
+    # https://earthscience.stackexchange.com/questions/5076/how-to-calculate-specific-humidity-with-relative-humidity-temperature-and-pres
+    # Td = math.log(e/6.112)*243.5/(17.67-math.log(e/6.112))
+    return round(q_, 2)
 
-    #Td = math.log(e/6.112)*243.5/(17.67-math.log(e/6.112))
-    es = 6.112 * math.exp((17.67 * t)/(t + 243.5))
-    e = es * (rh / 100)
-    q_ = (0.622 * e)/(p - (0.378 * e)) * 1000
-    return round(q_,2)
-
-def rh2ah(rh, t ):
+def rh2ah(rh, t):
+    # https://carnotcycle.wordpress.com/2012/08/04/how-to-convert-relative-humidity-to-absolute-humidity/
     return (6.112 * math.exp((17.67 * t) / (t + 243.5)) * rh * 2.1674) / (273.15 + t)
 
 def main():
-    rh =      st.sidebar.number_input(
-            "Relative humidity %", None, None, 36
-        )
-    t =     total_cases_0 = st.sidebar.number_input(
-            "Temperature (Celcius)", None, None, 8000
-        )
-    p =     total_cases_0 = st.sidebar.number_input(
-            "Presure", None, None, 1020
-        )
+    st.header("Calculate specfiic and absolute humidity")
+    t = st.sidebar.number_input("Temperature (Celcius)", None, None, 25)
+    rh = st.sidebar.number_input("Relative humidity %", None, None, 36)
+    t = st.sidebar.number_input("Temperature (Celcius)", None, None, 8000)
+    p = st.sidebar.number_input("Presure", None, None, 1020)
 
+    st.write(f"Specific humidity (q) = {round(rh2q(rh, t, p ),1)} g/kg")
+    st.write(f"Absolute humidity  = {round(rh2ah(rh, t),1)} grams/m3")
 
-    st.write (f"Specific humidity (q) = {rh2q(rh, t, p )}")
-    st.write (f"Absolute humidity  = {rh2ah(rh, t)}")
-
-    st.subheader ("Formula for specific humidity")
-    st.write ("es = 6.112 * math.exp((17.67 * t)/(t + 243.5))<br>e = es * (rh / 100)<br>q = (0.622 * e)/(p - (0.378 * e)) * 1000")
+    st.subheader("Formula for specific humidity")
+    st.write(
+        "es = 6.112 * exp((17.67 * t)/(t + 243.5))<br>e = es * (rh / 100)<br>q = (0.622 * e)/(p - (0.378 * e)) * 1000"
+    )
     st.subheader("Formula for absolute humidity")
-    st.write ("ah = 6.112 * math.exp((17.67 * t) / (t + 243.5)) * rh * 2.1674) / (273.15 + t)")
+    st.write(
+        "ah = 6.112 * exp((17.67 * t) / (t + 243.5)) * rh * 2.1674) / (273.15 + t)"
+    )
 
 if __name__ == "__main__":
-
     main()
-
-
