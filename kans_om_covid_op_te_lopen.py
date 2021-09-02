ba5c25328f4c77ffb41144b741edcb998b65abc3
rcsmit <rcx.smit@gmail.com>
2021-09-03 01:27:08 +0200

    .

--- kans_om_covid_op_te_lopen.py
+++ kans_om_covid_op_te_lopen.py
@@ -20,32 +20,32 @@ def main():
     st.sidebar.write (f"Kans om besmet te worden per jaar met bonussen : {kans_jaar}%")
     x_ax, y_ax = [],[]
 
-    col1,col2 = st.columns([1,3])
-    with col1:
-        st.write (f"Kans om het >=1 keer op te lopen in x jaar:" )
-        y = round((100 - (bereken_kans_periode(kans_jaar,1)*100)),2)
-        st.write (f"1 jaar - {y}")
-        x_ax.append(1)
+    #col1,col2 = st.columns([1,3])
+    #with col1:
+    st.write (f"Kans om het >=1 keer op te lopen in x jaar:" )
+    y = round((100 - (bereken_kans_periode(kans_jaar,1)*100)),2)
+    st.write (f"1 jaar - {y} %")
+    x_ax.append(1)
+    y_ax.append(y)
+
+
+
+    for x in range (5,50,5):
+        y = round((100 - (bereken_kans_periode(kans_jaar,x)*100)),2)
+
+        st.write (f" {x} jaar - {y} %")
+        x_ax.append(x)
         y_ax.append(y)
 
+    #with col2:
 
-
-        for x in range (5,50,5):
-            y = round((100 - (bereken_kans_periode(kans_jaar,x)*100)),2)
-
-            st.write (f" {x} jaar - {y}")
-            x_ax.append(x)
-            y_ax.append(y)
-
-    with col2:
-
-        with _lock:
-            fig1x = plt.figure()
-            ax = fig1x.add_subplot(111)
-            plt.plot(x_ax,y_ax)
-            plt.grid()
-            plt.title ("Kans om COVID op te lopen in x jaar")
-            st.pyplot(fig1x)
+    with _lock:
+        fig1x = plt.figure()
+        ax = fig1x.add_subplot(111)
+        plt.plot(x_ax,y_ax)
+        plt.grid()
+        plt.title ("Kans om COVID op te lopen in x jaar")
+        st.pyplot(fig1x)
     st.write ("Geinspireerd door Roel Griffioen: https://twitter.com/roelgrif/status/1433215573912080388")
 main()
 
