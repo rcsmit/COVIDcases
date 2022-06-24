4cdec71d1e1b990394c1708c2e9b15adde61281a
Rene Smit <rcx.smit@gmail.com>
2022-06-24 12:12:08 +0200

    .

--- oversterfte.py
+++ oversterfte.py
@@ -1,6 +1,25 @@
 # Imitating RIVM overstefte grafieken
 # overlijdens per week: https://opendata.cbs.nl/#/CBS/nl/dataset/70895ned/table?ts=1655808656887
 
+# Het verwachte aantal overledenen wanneer er geen COVID-19-epidemie was geweest, is
+# geschat op basis van de waargenomen sterfte in 2015–2019. Eerst wordt voor elk jaar de
+# sterfte per week bepaald. Vervolgens wordt per week de gemiddelde sterfte in die week
+# en de zes omliggende weken bepaald. Deze gemiddelde sterfte per week levert een
+# benadering van de verwachte wekelijkse sterfte. Er is dan nog geen rekening gehouden
+# met de trendmatige vergrijzing van de bevolking. Daarom is de sterfte per week nog
+# herschaald naar de verwachte totale sterfte voor het jaar. Voor 2020 is de verwachte sterfte
+# 153 402 en voor 2021 is deze 154 887. Het aantal voor 2020 is ontleend aan de
+# Kernprognose 2019–2060 en het aantal voor 2021 aan de Bevolkingsprognose 2020–2070
+# CBS en RIVM | Sterfte en oversterfte in 2020 en 2021 | Juni 2022 15
+# (exclusief de aanname van extra sterfgevallen door de corona-epidemie). De marges rond
+# de verwachte sterfte zijn geschat op basis van de waargenomen spreiding in de sterfte per
+# week in de periode 2015–2019. Dit 95%-interval geeft de bandbreedte weer van de
+# gewoonlijk fluctuaties in de sterfte. 95 procent van de sterfte die in eerdere jaren is
+# waargenomen, valt in dit interval. Er wordt van oversterfte gesproken wanneer de sterfte
+# boven de bovengrens van dit interval ligt.
+
+
+
 import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
@@ -8,18 +27,47 @@ import numpy as np
 
 def get_data_for_series(seriename):
     file = r"https://raw.githubusercontent.com/rcsmit/COVIDcases/main/input/overlijdens_per_week.csv"
-    try:
-        df_ = pd.read_csv(
-            file,
-            delimiter=";",
+    #try:
+    df_ = pd.read_csv(
+        file,
+        delimiter=";",
+        
+        low_memory=False,
+    )
+    df = df_[["jaar","weeknr","aantal_dgn",seriename]].copy(deep=True)
+    
+    df = df[(df["aantal_dgn"] == 7) & (df["jaar"] > 2014)]
+    #df = df[df["jaar"] > 2014 | (df["weeknr"] != 0) | (df["weeknr"] != 53)]
+    df = df.sort_values(by=['jaar','weeknr']).reset_index()
+ 
+
+    # Voor 2020 is de verwachte sterfte 153 402 en voor 2021 is deze 154 887.
+    # serienames = ["totaal_m_v_0_999","totaal_m_0_999","totaal_v_0_999","totaal_m_v_0_65","totaal_m_0_65","totaal_v_0_65","totaal_m_v_65_80","totaal_m_65_80","totaal_v_65_80","totaal_m_v_80_999","totaal_m_80_999","totaal_v_80_999"]
+
+    for y in range (2015,2020):
+        df_year = df[(df["jaar"] == y)]
+        som = df_year[seriename].sum()
+      
+        factor_2020 = 153402 / som
+        factor_2021 = 154887 / som
+        factor_2022 = 154887 / som
+       
+        for i in range(len(df)):
             
-            low_memory=False,
-        )
-        df = df_[["jaar","weeknr","aantal_dgn",seriename]].copy(deep=True)
-        df = df[df["aantal_dgn"] == 7]
-    except:
-        print("Error reading data")
-        st.stop()
+            
+            if df.loc[i,"jaar"] == y:
+                #for s in serienames:
+                new_column_name_2020 = seriename + "_factor_2020"
+                new_column_name_2021 = seriename + "_factor_2021"
+                new_column_name_2022 = seriename + "_factor_2022"
+                df.loc[i,new_column_name_2020] = df.loc[i,seriename] * factor_2020
+                df.loc[i,new_column_name_2021] = df.loc[i,seriename] * factor_2021               
+                df.loc[i,new_column_name_2022] = df.loc[i,seriename] * factor_2022
+    # except:
+    #     print("Error reading data")
+    #     st.stop()
+    
+  
     return df
 
 
@@ -27,47 +75,41 @@ def plot(series_names, how):
 
     for col, series_name in enumerate(series_names):
         print (f"---{series_name}----")
-        df_data = get_data_for_series(series_name)
+        df_data = get_data_for_series(series_name).copy(deep=True)
         
         if how =="quantiles":
             df_corona_20 = df_data[(df_data["jaar"] ==2020)].copy(deep=True)
             df_corona_21 = df_data[(df_data["jaar"] ==2021)].copy(deep=True)
+            df_corona_22 = df_data[(df_data["jaar"] ==2022)].copy(deep=True)
+            df_corona = pd.concat([df_corona_20, df_corona_21,  df_corona_22],axis = 0)
+            df_corona["weeknr"] = df_corona["jaar"].astype(str) +"_" + df_corona["weeknr"].astype(str).str.zfill(2)
+            print (df_corona)
+
+
             
-            df_to_use = df_data[(df_data["jaar"] !=2020) & (df_data["jaar"] !=2021)].copy(deep=True)
-            df_quantile = pd.DataFrame(
-                {"": [],  "q10": [], "q25": [], "q50":[] ,"avg": [], "q75": [], "q90": []}    )
-            
-            week_list = df_to_use['weeknr'].unique().tolist()
-            
-            for w in week_list:
+            df_quantile_2020 = make_df_quantile(series_name, df_data, 2020)
+            df_quantile_2021 = make_df_quantile(series_name, df_data, 2021)
+            df_quantile_2022 = make_df_quantile(series_name, df_data, 2022)
+            df_quantile = pd.concat([df_quantile_2020, df_quantile_2021,  df_quantile_2022],axis = 0)
+            columnlist = ["q05","q25","q50","avg","q75","q95", "low05", "high95"]
+            for what_to_sma in columnlist:
+                df_quantile[what_to_sma] = df_quantile[what_to_sma].rolling(window=6, center=True).mean()
 
-                df_to_use_ = df_to_use[(df_to_use["weeknr"] == w)].copy(deep=True)
-                data = df_to_use_[series_name] #.tolist()
-                q05 = np.percentile(data, 5)
-                q25 = np.percentile(data, 25)
-                q50 = np.percentile(data, 50)
-                q75 = np.percentile(data, 75)
-                q95 = np.percentile(data, 95)
-               
-                avg = data.mean()
-
-
-                df_quantile = df_quantile.append(
-                    {
-                        "week_": w,
-                        "q05": q05,
-                        "q25": q25,
-                        "q50": q50,
-                        "avg": avg,
-
-                        "q75": q75,
-
-                        "q95": q95,
-                        },
-                    ignore_index=True,
-                )
+            df_quantile["week_"]= df_quantile["jaar"].astype(str) +"_" + df_quantile['week_'].astype(str).str.zfill(2)
+                
             print (df_quantile)
+            df_quantile = df_quantile.sort_values(by=['jaar','week_'])
             fig = go.Figure()
+            low05 = go.Scatter(
+                name='low05',
+                x=df_quantile["week_"],
+                y=df_quantile['low05'] ,
+                mode='lines',
+                line=dict(width=0.5,
+                        color="rgba(255, 188, 0, 0.5)"),
+                fillcolor='rgba(68, 68, 68, 0.1)', fill='tonexty')
+
+
             q05 = go.Scatter(
                 name='q05',
                 x=df_quantile["week_"],
@@ -75,7 +117,7 @@ def plot(series_names, how):
                 mode='lines',
                 line=dict(width=0.5,
                         color="rgba(255, 188, 0, 0.5)"),
-                fillcolor='rgba(68, 68, 68, 0.1)', fill='tonexty')
+                fillcolor='rgba(68, 68, 68, 0.2)', fill='tonexty')
 
             q25 = go.Scatter(
                 name='q25',
@@ -84,7 +126,7 @@ def plot(series_names, how):
                 mode='lines',
                 line=dict(width=0.5,
                         color="rgba(255, 188, 0, 0.5)"),
-                fillcolor='rgba(68, 68, 68, 0.2)',
+                fillcolor='rgba(68, 68, 68, 0.3)',
                 fill='tonexty')
 
             avg = go.Scatter(
@@ -95,20 +137,14 @@ def plot(series_names, how):
                 line=dict(width=0.75,color='rgba(68, 68, 68, 0.8)'),
                 )
 
-            value_in_year_2020 = go.Scatter(
-                name="2020",
-                x=df_corona_20["weeknr"],
-                y=df_corona_20[series_name],
+            sterfte = go.Scatter(
+                name="Sterfte",
+                x=df_corona["weeknr"],
+                y=df_corona[series_name],
                 mode='lines',
                 line=dict(width=2,color='rgba(255, 0, 0, 0.8)'),
                 )
-            value_in_year_2021 = go.Scatter(
-                name="2021",
-                x=df_corona_21["weeknr"],
-                y=df_corona_21[series_name],
-                mode='lines',
-                line=dict(width=2,color='rgba(0, 0, 255, 0.8)'),
-                )
+    
 
             q75 = go.Scatter(
                 name='q75',
@@ -117,7 +153,7 @@ def plot(series_names, how):
                 mode='lines',
                 line=dict(width=0.5,
                         color="rgba(255, 188, 0, 0.5)"),
-                fillcolor='rgba(68, 68, 68, 0.1)',
+                fillcolor='rgba(68, 68, 68, 0.3)',
                 fill='tonexty')
 
 
@@ -128,10 +164,19 @@ def plot(series_names, how):
                 mode='lines',
                 line=dict(width=0.5,
                         color="rgba(255, 188, 0, 0.5)"),
-                fillcolor='rgba(68, 68, 68, 0.1)'
+                fillcolor='rgba(68, 68, 68, 0.3)'
             )
-
-            data = [q95, q75, q25, q05,avg, value_in_year_2020, value_in_year_2021 ]
+            high95 = go.Scatter(
+                name='high95',
+                x=df_quantile["week_"],
+                y=df_quantile['high95'],
+                mode='lines',
+                line=dict(width=0.5,
+                        color="rgba(255, 188, 0, 0.5)"),
+                fillcolor='rgba(68, 68, 68, 0.2)'
+             )
+            
+            data = [ q95, high95, q05,low05,avg, sterfte] #, value_in_year_2021 ]
             title = f"Overleden {series_name}"
             layout = go.Layout(xaxis=dict(title="Weeknumber"),yaxis=dict(title="Number of persons"),
                             title=title,)
@@ -139,6 +184,29 @@ def plot(series_names, how):
     
             fig = go.Figure(data=data, layout=layout)
             fig.update_layout(xaxis=dict(tickformat="%d-%m"))
+            
+            #             — eerste oversterftegolf: week 13 tot en met 18 van 2020 (eind maart–eind april 2020);
+            # — tweede oversterftegolf: week 39 van 2020 tot en met week 3 van 2021 (eind
+            # september 2020–januari 2021);
+            # — derde oversterftegolf: week 33 tot en met week 52 van 2021 (half augustus 2021–eind
+            # december 2021).
+            # De hittegolf in 2020 betreft week 33 en week 34 (half augustus 2020).
+
+            fig.add_vrect(x0="2020_13", x1="2020_18", 
+              annotation_text="Eerste golf", annotation_position="top left",
+              fillcolor="pink", opacity=0.25, line_width=0)
+            fig.add_vrect(x0="2020_39", x1="2021_3", 
+              annotation_text="Tweede golf", annotation_position="top left",
+              fillcolor="pink", opacity=0.25, line_width=0)
+            fig.add_vrect(x0="2021_33", x1="2021_52", 
+              annotation_text="Derde golf", annotation_position="top left",
+              fillcolor="pink", opacity=0.25, line_width=0)
+            fig.add_vrect(x0="2020_33", x1="2020_34", 
+              annotation_text="Hitte golf", annotation_position="top left",
+              fillcolor="orange", opacity=0.25, line_width=0)
+
+
+
             st.plotly_chart(fig, use_container_width=True)
 
         else:
@@ -156,7 +224,7 @@ def plot(series_names, how):
                 else:
                     width = .7
                     opacity = .3
-
+                
                 fig_ = go.Scatter(x=df['weeknr'],
                             y=df[series_name],
                             line=dict(width=width), opacity = opacity, # PLOT_COLORS_WIDTH[year][1] , color=PLOT_COLORS_WIDTH[year][0]),
@@ -173,9 +241,57 @@ def plot(series_names, how):
     
             fig = go.Figure(data=data, layout=layout)
             st.plotly_chart(fig, use_container_width=True)
+
+def make_df_quantile(series_name, df_data, year):
+    df_to_use = df_data[(df_data["jaar"] !=2020) & (df_data["jaar"] !=2021) & (df_data["jaar"] !=2022)].copy(deep=True)
+    df_quantile =None
+           
+    week_list = df_to_use['weeknr'].unique().tolist()
+            # week_list = week_list.sort()
+          
+            
+            #for w in week_list:  #puts week 1 at the end
+    for w in range(1,53):
+        df_to_use_ = df_to_use[(df_to_use["weeknr"] == w)].copy(deep=True)
+        column_to_use = series_name +  "_factor_" + str(year)
+        data = df_to_use_[column_to_use ] #.tolist()
+               
+        q05 = np.percentile(data, 5)
+        q25 = np.percentile(data, 25)
+        q50 = np.percentile(data, 50)
+        q75 = np.percentile(data, 75)
+        q95 = np.percentile(data, 95)
+               
+        avg = data.mean()
+        sd = data.std()
+        low05 = avg - (2*sd)
+        high95 = avg +(2*sd)
+       
+
+
+        df_quantile_ =  pd.DataFrame(
+                   [ {
+                        "week_": w,
+                        "jaar":year,
+                        "q05": q05,
+                        "q25": q25,
+                        "q50": q50,
+                        "avg": avg,
+                        "q75": q75,
+                        "q95": q95,
+                        "low05":low05,
+                        "high95":high95
+                        }]
+                )
+
+        df_quantile = pd.concat([df_quantile, df_quantile_],axis = 0)
+        
+    return df_quantile
         
 def main():
-    serienames = ["totaal_m_v_0_999","totaal_m_0_999","totaal_v_0_999","totaal_m_v_0_65","totaal_m_0_65","totaal_v_0_65","totaal_m_v_65_80","totaal_m_65_80","totaal_v_65_80","totaal_m_v_80_999","totaal_m_80_999","totaal_v_80_999"]
+    #serienames = ["totaal_m_v_0_999","totaal_m_0_999","totaal_v_0_999","totaal_m_v_0_65","totaal_m_0_65","totaal_v_0_65","totaal_m_v_65_80","totaal_m_65_80","totaal_v_65_80","totaal_m_v_80_999","totaal_m_80_999","totaal_v_80_999"]
+    
+    serienames = ["totaal_m_v_0_999"]
     how = st.sidebar.selectbox("How", ["quantiles", "Lines"], index = 0)
     plot(serienames, how)
 
