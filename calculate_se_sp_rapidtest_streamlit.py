d566304c241469c2a60035fb2396a7a6bdf17a99
Rene Smit <rcx.smit@gmail.com>
2022-07-24 13:29:23 +0200

    .

--- calculate_se_sp_rapidtest_streamlit.py
+++ calculate_se_sp_rapidtest_streamlit.py
@@ -7,126 +7,17 @@ from tabulate import tabulate
 import streamlit as st
 import math
 from statsmodels.stats.proportion import proportion_confint
+import scipy.stats as scist
 
-def calculate_se_sp():
-    col1, col2,col3,col4 = st.columns(4)
-    with col1: 
-        st.write("")
-    with col2:
-        st.write("PCR +")
-    with col3:
-        st.write("PCR -")
-    with col3:
-        st.write("Total")
-
-    col1, col2,col3,col4 = st.columns(4)
-    with col1: 
-        st.write("")
-        st.write("")
-        st.write("")
-        st.write("Rapid +")
-    with col2:
-        tp =  (st.number_input('True positive',None,None, 98))
-    with col3:
-        fp =  (st.number_input('False positive',None,None, 0))
-    with col4:   
-        st.write("")
-        st.write("")
-
-        st.write("")
-        st.write(tp+fp)
-    col1, col2,col3,col4 = st.columns(4)
-    with col1: 
-
-        st.write("")
-        st.write("")
-        st.write("")
-        st.write("Rapid -")
-    with col2:
-        fn =  (st.number_input('False negative',None,None, 2))
-    with col3:
-        tn =  (st.number_input('True Negative',None,None, 210))
-    with col4:  
-        st.write("")
-        st.write("")
-        st.write("") 
-        st.write(fn+tn)
-
-
-    col1, col2,col3,col4 = st.columns(4)
-    with col1: 
-        st.write("")
-    with col2:
-        st.write(tp+fn)
-    with col3:
-        st.write(fp+tn)
-    with col4:   
-        st.write(tp+fn+fp+tn )
-
-
-
-    se = tp/(tp+fn)*100
-    sp = tn/(fp+tn)*100
-    acc = (tp + tn) /(tp+fn+fp+tn)*100
-    alfa = 0.05
-
-    #  SE sensitivity = square root [sensitivity – (1-sensitivity)]/n sensitivity) https://www.ncbi.nlm.nih.gov/books/NBK305683/
-    Za2 = 1.96
-    SE_sp = math.sqrt((sp/100*(1-sp/100))/(fp+tn) )  #/(tp+fn)  #https://www2.ccrb.cuhk.edu.hk/stat/confidence%20interval/Diagnostic%20Statistic.htm#Formula
- 
-
-    c1se,c2se = simple_asymptotic(tp,(tp+fn))
-    c1sp,c2sp = simple_asymptotic(tn,(fp+tn))
-    c1,c2 = binomial_ci(tp,(tp+fn), alpha=0.05) #exact (Clopper-Pearson)
-    c1y,c2y = binomial_ci(tn,(fp+tn), alpha=0.05) # exact (Clopper-Pearson)
-    # https://www2.ccrb.cuhk.edu.hk/stat/confidence%20interval/Diagnostic%20Statistic.htm
-    # https://www.medcalc.org/calc/diagnostic_test.php
-
-    # https%3A%2F%2Fncss-wpengine.netdna-ssl.com%2Fwp-content%2Fthemes%2Fncss%2Fpdf%2FProcedures%2FPASS%2FConfidence_Intervals_for_One-Sample_Sensitivity_and_Specificity.pdf
-    sp = tn/(fp+tn)*100
-    acc = (tp + tn) /(tp+fn+fp+tn)*100
-    st.write(f" Sensitivity = {tp}/({tp}+{fn})*100% = {round(se,2)}")
-    st.write(f"95% CI")
-    st.write(f"Simple Asymptotic: [{round(c1se*100,1)} - {round(c2se*100,1)}]")
-    st.write(f"exact (Clopper-Pearson) = beta): [{round(c1*100,1)} - {round(c2*100,1)}]")
-    calculate_confint(tp,(tp+fn), alpha=0.05)
-    st.write("")
-    st.write(f" Specificity = {tn}/({fp}+{tn})*100% = {round(sp,2)}")
-    st.write(f"95% CI")
-    st.write(f"Simple Asymptotic: [{round(c1sp*100,1)} - {round(c2sp*100,1)}]")
-    st.write(f"Exact (Clopper-Pearson) = beta): [{round(c1y*100,1)} - {round(c2y*100,1)}] ")
-    calculate_confint(tn,(fp+tn), alpha=0.05)
-    st.write("")
-    st.write(f" Accuracy  =  ({tp} + {tn})/ ({tp}+{fn}+{fp}+{tn})*100% = {round(acc,2)}")
-    calculate_confint((tn+tp),(fp+tn+tp+fn), alpha=0.05)
-        # https://www2.ccrb.cuhk.edu.hk/stat/confidence%20interval/Diagnostic%20Statistic.htm
-        # The Specificity is 0.91 and the 95% C.I. is (0.89746, 0.92254).
-
-        # The Positive Predictive Value (PPV) is 0.1 and the 95% C.I. is (0.05842, 0.14158).
-
-        # The Negative Predictive Value (NPV) is 0.99454 and the 95% C.I. is (0.99116, 0.99791).
-
-        # The Pre-Test Probability is 0.01478.
-
-        # The Likelihood Ratio Positive (LR+) is 7.40741 and the 95% C.I. is (5.54896, 9.88828).
-
-        # The Positive Post-Test Probability is 0.1.
-
-        # The Likelihood Ratio Negative (LR-) is 0.3663 and the 95% C.I. is (0.22079, 0.60771).
-
-        # The Negative Post-Test Probability is 0.00546.
-
-    return se,sp,acc
-
-def simple_asymptotic(x,n):
-    # Simple Asymptotic
+def simple_asymptotic(x,n, Za2):
+    # Simple Asymptotic = Wald
     # The simple asymptotic formula is based on the normal approximation to the binomial distribution. 
     # The approximation is close only for very large sample sizes.
  
     p = x/n
     SE = math.sqrt((p*(1-p))/n )   #https://www2.ccrb.cuhk.edu.hk/stat/confidence%20interval/Diagnostic%20Statistic.htm#Formula
-    c1 = round( p - 1.96*SE,3)
-    c2 = round ( p + 1.96*SE,3)
+    c1 = round( p - Za2*SE,3)
+    c2 = round ( p + Za2*SE,3)
 
     return c1, c2
   
@@ -135,17 +26,11 @@ def calculate_confint(x, n, alpha=0.05):
     # https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval
 
     # normal : asymptotic normal approximation
-
     # agresti_coull : Agresti-Coull interval
-
     # beta : Clopper-Pearson interval based on Beta distribution
-
     # wilson : Wilson Score interval
-
     # jeffreys : Jeffreys Bayesian Interval
-
     # binom_test : Numerical inversion of binom_test
-
 
     # VEEL ONDERZOEKEN LATEN BETA ZIEN
     # Flowflex gebruikt agresti-coull
@@ -186,6 +71,68 @@ def binomial_ci(x, n, alpha=0.05):
 # https://andrewpwheeler.com/2020/11/30/confidence-intervals-around-proportions/
 
 
+
+def calculate_se_sp( tp, fp, fn, tn, alpha ):
+    
+    """Calculte the Sensitivity and Specificity with it CI's
+
+    Returns:
+        se, sp, acc: sensitivity and specificity 
+    """
+
+
+    se = tp/(tp+fn)*100
+    sp = tn/(fp+tn)*100
+    acc = (tp + tn) /(tp+fn+fp+tn)*100
+    
+
+    #  SE sensitivity = square root [sensitivity – (1-sensitivity)]/n sensitivity) https://www.ncbi.nlm.nih.gov/books/NBK305683/
+    
+    SE_sp = math.sqrt((sp/100*(1-sp/100))/(fp+tn) )  #/(tp+fn)  #https://www2.ccrb.cuhk.edu.hk/stat/confidence%20interval/Diagnostic%20Statistic.htm#Formula
+    col1,col2,col3 = st.columns(3)
+    with col1:
+        st.subheader("Sensitivity")
+        se = calulate_value_and_ci ("Sensitivity", tp,(tp+fn), alpha)
+    with col2:
+        st.subheader(f"Specificity")
+        sp = calulate_value_and_ci ("Specificity", tn,(fp+tn), alpha)
+    with col3:
+        st.subheader(f" Accuracy")
+        acc = calulate_value_and_ci ("Accuracy",(tp + tn), (tp+fn+fp+tn),alpha)
+
+    return se, sp, acc
+
+def calulate_value_and_ci(what, m,n, alpha):
+    Za2 = scist.norm.ppf(1-(alpha/2))
+    c1,c2 = simple_asymptotic(m,n, Za2)
+    bin_c1,bin_c2 = binomial_ci(m,n, alpha=alpha) #exact (Clopper-Pearson)
+    # https://www2.ccrb.cuhk.edu.hk/stat/confidence%20interval/Diagnostic%20Statistic.htm
+    # https://www.medcalc.org/calc/diagnostic_test.php
+
+    # https%3A%2F%2Fncss-wpengine.netdna-ssl.com%2Fwp-content%2Fthemes%2Fncss%2Fpdf%2FProcedures%2FPASS%2FConfidence_Intervals_for_One-Sample_Sensitivity_and_Specificity.pdf
+    value = m/(n)*100
+    st.write(f"{what} = {value} %")
+   
+   
+    st.write(f"Simple Asymptotic (Wald):")
+    st.write (f"[{round(c1*100,1)} - {round(c2*100,1)}]")
+    st.write(f"exact (Clopper-Pearson) = beta):") #geeft andere waarde
+    st.write(f"[{round(bin_c1*100,1)} - {round(bin_c2*100,1)}]")
+    calculate_confint(m,n, alpha=alpha)
+    st.write("")
+
+        # https://www2.ccrb.cuhk.edu.hk/stat/confidence%20interval/Diagnostic%20Statistic.htm
+        # The Specificity is 0.91 and the 95% C.I. is (0.89746, 0.92254).
+        # The Positive Predictive Value (PPV) is 0.1 and the 95% C.I. is (0.05842, 0.14158).
+        # The Negative Predictive Value (NPV) is 0.99454 and the 95% C.I. is (0.99116, 0.99791).
+        # The Pre-Test Probability is 0.01478.
+        # The Likelihood Ratio Positive (LR+) is 7.40741 and the 95% C.I. is (5.54896, 9.88828).
+        # The Positive Post-Test Probability is 0.1.
+        # The Likelihood Ratio Negative (LR-) is 0.3663 and the 95% C.I. is (0.22079, 0.60771).
+        # The Negative Post-Test Probability is 0.00546.
+
+    return value
+
 def calculate(se, sp, prevalentie, number_of_tested_people,  output):
     """calculates the values. Made as a function to be able to make graphs
     with various prevalences
@@ -200,7 +147,8 @@ def calculate(se, sp, prevalentie, number_of_tested_people,  output):
         fdr,for_, pos, fpr - see code
     """
    
-    sensitivity = se/100      
+    sensitivity = se/100    
+ 
     specificity = sp/100
 
     total_healthy = (100 - prevalentie) * number_of_tested_people/100
@@ -216,10 +164,12 @@ def calculate(se, sp, prevalentie, number_of_tested_people,  output):
     # print (f"{total_healthy=}")
     # print (f"{false_positive=}")
 
-    true_positive_bayes = round (100* (sensitivity * prevalentie) / ((sensitivity * prevalentie) + ((1-specificity)* (1-prevalentie)  )),2)
-
-    fdr = round(100*false_positive/(false_positive+ true_positive),4)
-    #print (fdr)
+    #true_positive_bayes = round (100* (sensitivity * prevalentie) / ((sensitivity * prevalentie) + ((1-specificity)* (1-prevalentie)  )),2)
+    try:
+        fdr = round(100*false_positive/(false_positive+ true_positive),4)
+        #print (fdr)
+    except:
+        fdr = 0
     acc = round(100*((true_positive+true_negative)/number_of_tested_people),4)
     for_ = round(100*false_negative/(false_negative + true_negative),4)
     try:
@@ -231,7 +181,10 @@ def calculate(se, sp, prevalentie, number_of_tested_people,  output):
     except:
         fnr = 0
     pos = round(100*((true_positive + false_positive)/number_of_tested_people),4)
-    ppv = round((true_positive/(true_positive+false_positive)*100),3)
+    try:
+        ppv = round((true_positive/(true_positive+false_positive)*100),3)
+    except:
+        ppv = 0
     npv = round((true_negative/(false_negative+true_negative)*100),3)
     tpr =round(100*true_positive/(true_positive+false_negative),3) # equal to Se
     try:
@@ -362,22 +315,80 @@ def calculate(se, sp, prevalentie, number_of_tested_people,  output):
     return fdr,for_, pos, fpr
 
 
+def interface_test_results():
+    col1, col2,col3,col4 = st.columns(4)
+    with col1: 
+        st.write("")
+    with col2:
+        st.write("PCR +")
+    with col3:
+        st.write("PCR -")
+    with col3:
+        st.write("Total")
 
-def interface():
+    col1, col2,col3,col4 = st.columns(4)
+    with col1: 
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("Rapid +")
+    with col2:
+        tp =  (st.number_input('True positive',None,None, 98))
+    with col3:
+        fp =  (st.number_input('False positive',None,None, 0))
+    with col4:   
+        st.write("")
+        st.write("")
+
+        st.write("")
+        st.write(tp+fp)
+    col1, col2,col3,col4 = st.columns(4)
+    with col1: 
+        st.write("")
+        st.write("")
+        st.write("")
+        st.write("Rapid -")
+    with col2:
+        fn =  (st.number_input('False negative',None,None, 2))
+    with col3:
+        tn =  (st.number_input('True Negative',None,None, 210))
+    with col4:  
+        st.write("")
+        st.write("")
+        st.write("") 
+        st.write(fn+tn)
+
+
+    col1, col2,col3,col4 = st.columns(4)
+    with col1: 
+        st.write("")
+    with col2:
+        st.write(tp+fn)
+    with col3:
+        st.write(fp+tn)
+    with col4:   
+        st.write(tp+fn+fp+tn )
+    return tp,fp,fn,tn
+
+def interface_left_bar():
     prevalentie = st.sidebar.number_input('prevalence testpopulation in %',0.0,100.0, 1.0, format="%.4f")
     number_of_tested_people =  (st.sidebar.number_input('Number of tested people',None,None, 100_000))
+    alpha =  (st.sidebar.number_input('Number of tested people',None,None, 0.05))
+        
     st.sidebar.write("Attention: too small numbers give erorrs (Division by zero)")
-    return prevalentie,number_of_tested_people
+    return prevalentie,number_of_tested_people, alpha
 
 
 
 def main():
     
     st.title(" Calculating Sensitivity and Specificity of Rapid tests")
+    tp, fp, fn, tn = interface_test_results()
+    prevalentie, number_of_tested_people, alpha = interface_left_bar()
 
-    se,sp,acc =  calculate_se_sp()
-    prevalentie, number_of_tested_people = interface()
-    fdr, for_, pos, fpr = calculate(se,sp, prevalentie, number_of_tested_people, True)
+    se,sp, acc =  calculate_se_sp(tp, fp, fn, tn, alpha)
+ 
+    fdr, for_, pos, fpr = calculate( se,sp, prevalentie, number_of_tested_people, True)
 
 
 if __name__ == "__main__":
