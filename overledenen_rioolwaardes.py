c06bd8b4ceb7549de6fa013ef33bc4a74c8725ca
Rene Smit <rcx.smit@gmail.com>
2023-01-02 23:59:04 +0700

    .

--- overledenen_rioolwaardes.py
+++ overledenen_rioolwaardes.py
@@ -150,10 +150,13 @@ def make_plot(df_,cat, what_,jaar,rw, show_interval):
                                 title=title,)
                 
         fig.add_hline(y=0)
-        fig.add_vrect(x0="2020_33", x1="2020_34", 
+        # fig.add_vrect(x0="2020_33", x1="2020_34", 
+        #       annotation_text="Hitte golf", annotation_position="top left",
+        #       fillcolor="orange", opacity=0.5, line_width=0)
+
+        fig.add_vrect(x0="2022_32", x1="2020_33", 
               annotation_text="Hitte golf", annotation_position="top left",
               fillcolor="orange", opacity=0.25, line_width=0)
-
         fig.update_yaxes(rangemode='tozero')
 
         st.plotly_chart(fig, use_container_width=True)
