bcb698370313fc9826bc8233ce13e9fbe072ff49
Rene Smit <rcx.smit@gmail.com>
2024-09-22 14:24:24 +0200

    ..

--- covid_menu_streamlit.py
+++ covid_menu_streamlit.py
@@ -80,7 +80,7 @@ def main():
             ["[50] Calculate baselines (Poisson)", "calculate_baselines"],
             ["[51] AG table mortality", "agtable_mortality"],
             ["[52] Find baseline length", "find_baseline_length"],
-            ["[52] Mortality/week/100k","mortality_weekly_per_age_per_capita.py"],
+            ["[52] Mortality/week/100k","mortality_weekly_per_age_per_capita"],
             ]
     
 
