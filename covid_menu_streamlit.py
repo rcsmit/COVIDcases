dab0c38fc00e1d0a1bf4831e32ebc38ca3a9bdd7
Rene Smit <rcx.smit@gmail.com>
2024-09-22 14:23:49 +0200

    ..

--- covid_menu_streamlit.py
+++ covid_menu_streamlit.py
@@ -82,6 +82,7 @@ def main():
             ["[52] Find baseline length", "find_baseline_length"],
             ["[52] Mortality/week/100k","mortality_weekly_per_age_per_capita.py"],
             ]
+    
 
     #query_params = st.experimental_get_query_params() # reading  the choice from the URL..
     # query_params = st.query_params["choice"] 
