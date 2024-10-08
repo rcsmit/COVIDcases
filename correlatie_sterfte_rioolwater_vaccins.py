a24fb3a99f95cea0c750e3d86e1bd27d84652f7a
Rene Smit <rcx.smit@gmail.com>
2024-10-08 02:36:59 +0200

    xx

--- correlatie_sterfte_rioolwater_vaccins.py
+++ correlatie_sterfte_rioolwater_vaccins.py
@@ -419,7 +419,8 @@ def multiple_linear_regression(df: pd.DataFrame, x_values: List[str], y_value_: 
     # residuals = [float(r.split()[1]) for r in data["Residuals"].split('\n') if r]
 
     # Stap 4: Omzetten naar een DataFrame
-    y_value_x =  f"{data["Y value"]}_{age_sex}"
+    xx=data["Y value"]
+    y_value_x =  f"{xx}_{age_sex}"
     df = pd.DataFrame({
         "Y value":y_value_x,
         # "Coefficients": pd.Series(coefficients),
