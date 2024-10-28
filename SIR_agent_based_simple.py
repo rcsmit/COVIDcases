1c9e0f9c6cd34f7f59422d199a6234a70821ae80
Rene Smit <rcx.smit@gmail.com>
2024-10-28 16:05:41 +0100

    ..

--- SIR_agent_based_simple.py
+++ SIR_agent_based_simple.py
@@ -14,75 +14,112 @@ class Agent:
         if self.state == 'S' and np.random.rand() < prob_infection:
             self.state = 'I'  # Move to Infected state
 
-    def update_state(self, prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic):
-        if self.state == 'I':
+    def update_state(self, prob_infection, prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic):
+        if self.state == 'S' and np.random.rand() < prob_infection:
+            self.state = 'I'  # Move to Infected state
+
+        elif self.state == 'I':
             if np.random.rand() < prob_hospitalization:
                 self.state = 'H'  # Hospitalized
                 self.days_in_hospital = np.random.poisson(avg_stay_hospital)
+                
             elif np.random.rand() < prob_recovery:
                 self.state = 'R'  # Recovered from Infection
 
         elif self.state == 'H':
             if self.days_in_hospital > 0:
                 self.days_in_hospital -= 1
+                if self.days_in_hospital <np.random.poisson(3):
+                    if np.random.rand() < prob_ic:
+                        self.state = 'IC'  # Move to Intensive Care
+                        self.days_in_ic = np.random.poisson(avg_stay_ic)
+                # elif np.random.rand() < prob_recovery:
+                #     self.state = 'R'  # Recovered from Hospitalization
             else:
-                if np.random.rand() < prob_ic:
-                    self.state = 'IC'  # Move to Intensive Care
-                    self.days_in_ic = np.random.poisson(avg_stay_ic)
-                elif np.random.rand() < prob_recovery:
-                    self.state = 'R'  # Recovered from Hospitalization
+                self.state = 'R'  # Recovered from Hospitalization
+
 
         elif self.state == 'IC':
             if self.days_in_ic > 0:
                 self.days_in_ic -= 1
-            else:
+
                 if np.random.rand() < prob_death:
                     self.state = 'D'  # Deceased
-                elif np.random.rand() < prob_recovery:
-                    self.state = 'R'  # Recovered from IC
+                # elif np.random.rand() < prob_recovery:
+                #     self.state = 'R'  # Recovered from IC
+            else:
 
-def simulate(population_size, prob_infection, new_prob_infection_factor, day_new_prob_infection_factor, prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic, steps):
+                self.state = 'R'  # Recovered from IC
+
+def simulate(population_size, prob_infection, new_prob_infection_factor, day_new_prob_infection_factor,
+             prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic, steps):
     
-   
     # Initialize population of agents
     population = [Agent() for _ in range(population_size)]
+    
     # Infect a few initial agents
-    for agent in np.random.choice(population, int(0.01 * population_size), replace=False):
-        agent.state = 'I'
+    initial_infections = int(0.01 * population_size)
+    number_infected = initial_infections
+    infected_agents_indices = np.random.choice(range(population_size), initial_infections, replace=False)
+    for idx in infected_agents_indices:
+        population[idx].state = 'I'
 
     # Track state counts
-    results = {'S': [], 'I': [], 'H': [], 'IC': [], 'R': [], 'D': [], 'Hospital_Occupation': [], 'IC_Occupation': []}
-    placeholder=st.empty()
-    for _ in range(steps):
-        placeholder.info(f"{_+1}/{steps}")
-        # Spread infection
-        infected_agents = [agent for agent in population if agent.state == 'I']
-        for agent in population:
-            if agent.state == 'S':
-                for infected in infected_agents:
-                    if steps >= day_new_prob_infection_factor:
-                        prob_infection = prob_infection * new_prob_infection_factor
-                    agent.infect(prob_infection)
+    results = {
+        'S': [population_size - initial_infections],
+        'I': [initial_infections],
+        'H': [0],
+        'IC': [0],
+        'R': [0],
+        'D': [0],
+        
+    }
+    
+    placeholder = st.empty()
+    
+    for step in range(steps):
+        
+
+        # Determine the effective probability of infection
+        if step >= day_new_prob_infection_factor:
+            effective_prob_infection = prob_infection * new_prob_infection_factor
+        else:
+            effective_prob_infection = prob_infection
+
+        # Vectorized infection process
+        susceptible_indices = [i for i, agent in enumerate(population) if agent.state == 'S']
+        infected_agents_indices = [i for i, agent in enumerate(population) if agent.state == 'I']
+        
+        for idx in susceptible_indices:
+            for infected_idx in infected_agents_indices:
+                if np.random.rand() < effective_prob_infection:
+                    population[idx].infect(prob_infection)
+                    break  # Stop checking once infected to reduce unnecessary checks
 
         # Update states of all agents
-        for agent in population:
-            agent.update_state(prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic)
+        for i,agent in enumerate(population):
+            # Calculate progress percentage
+            progress = (i + 1) / len(population) * 100
+
+            # Print statement every 10%
+            if progress % 10 == 0:
+                placeholder.info(f"agents: {i+1}/{len(population)} - days: {step + 1}/{steps}")
+            agent.update_state(prob_infection*number_infected, prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic)
 
         # Record the counts of each state
-        counts = {state: sum(1 for agent in population if agent.state == state) for state in ['S', 'I', 'H', 'IC', 'R', 'D']}
+        counts = {state: 0 for state in ['S', 'I', 'H', 'IC', 'R', 'D']}
+        for agent in population:
+            counts[agent.state] += 1
         
-        # Calculate hospital and IC occupation
-        hospital_occupation = counts['H']
-        ic_occupation = counts['IC']
-
-        # Update results dictionary
-        for key in results:
-            if key in counts:
-                results[key].append(counts[key])
-        results['Hospital_Occupation'].append(hospital_occupation)
-        results['IC_Occupation'].append(ic_occupation)
+       
+        # Update state counts in results
+        for state in ['S', 'I', 'H', 'IC', 'R', 'D']:
+            results[state].append(counts[state])
+        number_infected = counts["I"]
+       
     placeholder.empty()
     return results
+
 def main():
     # RIVM 2021
     names =  ["0-9", "10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70-79", "80+"]
@@ -118,9 +155,9 @@ def main():
     # Input parameters
     
     population_size = st.sidebar.number_input("Population Size", value=50000, min_value=1000)
+    prob_infection = st.sidebar.number_input("Lambda", value=0.00005, min_value=0.0, max_value=1.0, format="%.8f")
     new_prob_infection_factor = st.sidebar.number_input("New Probability Infection Factor", value=0.9, min_value=0.0, max_value=10.0)
     day_new_prob_infection_factor = st.sidebar.number_input("Day New Probability Infection", value=10, min_value=1)
-    prob_infection = st.sidebar.number_input("Probability of Infection from a Single Contact", value=0.061, min_value=0.0, max_value=1.0)
     prob_recovery = st.sidebar.number_input("General Recovery Probability", value=0.2, min_value=0.0, max_value=1.0)
     avg_stay_hospital = st.sidebar.number_input("Average Days in Hospital", value=10, min_value=1)
     avg_stay_ic = st.sidebar.number_input("Average Days in ICU", value=14, min_value=1)
@@ -131,32 +168,49 @@ def main():
     prob_hospitalization = calculate_weighted(h_)    # Probability of hospitalization for infected agents
     prob_ic = calculate_weighted(i1_)                # Probability of ICU admission from hospital
     prob_death = calculate_weighted(dic_)           # Probability of death from ICU
-    
+    st.sidebar.write(f"I- H : {round(prob_hospitalization,3)}")
+    st.sidebar.write(f"H- IC :{round(prob_ic,3)}")
+    st.sidebar.write(f"IC-D :{round(prob_death,3)}")
     # RIVM 2021 = The expected outcome of COVID-19 vaccination strategies, p.69
     # Klinkenberg, 2023 = Projecting COVID-19 intensive care admissions in the Netherlands for policy advice: 
     #                     February 2020 to January 2021, p.5
     # Run simulation
-    results = simulate(population_size, prob_infection,  new_prob_infection_factor, day_new_prob_infection_factor,prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic, steps)
+    col1,col2=st.columns(2)
+    with col1:
+        st.subheader("Without correction")    
+        results = simulate(population_size, prob_infection,  1, day_new_prob_infection_factor,prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic, steps)    
+        plot_graphs(steps, results)
+    with col2:
+        st.subheader("With correction")    
+        results = simulate(population_size, prob_infection,  new_prob_infection_factor, day_new_prob_infection_factor,prob_hospitalization, prob_ic, prob_recovery, prob_death, avg_stay_hospital, avg_stay_ic, steps)    
+        plot_graphs(steps, results)
 
-    # Plot results using Plotly
-    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1,
-                        subplot_titles=("SIR Model with Hospitalization and IC", "Hospital and IC Occupation Over Time"))
-
-    # First subplot: S, I, H, IC, R, D counts
-    for state in ['I', 'H', 'IC', 'R', 'D']:
+def plot_graphs(steps, results):
+    fig = make_subplots(rows=1, cols=1, shared_xaxes=True, vertical_spacing=0.3)
+    for state in ['I']:
         fig.add_trace(go.Scatter(x=list(range(steps)), y=results[state], mode='lines', name=state), row=1, col=1)
-
-    # Second subplot: Hospital and IC Occupation
-    fig.add_trace(go.Scatter(x=list(range(steps)), y=results['Hospital_Occupation'], mode='lines', name='Hospital Occupation', line=dict(color='purple')), row=2, col=1)
-    fig.add_trace(go.Scatter(x=list(range(steps)), y=results['IC_Occupation'], mode='lines', name='IC Occupation', line=dict(color='red')), row=2, col=1)
-
-    # Update layout
-    fig.update_layout(title_text="Agent-Based SIR Model with Hospital and IC Occupation")
-    fig.update_xaxes(title_text="Time Step", row=2, col=1)
-    fig.update_yaxes(title_text="Number of Agents", row=1, col=1)
-    fig.update_yaxes(title_text="Occupation Count", row=2, col=1)
-
+        fig.update_layout(title_text="Infected")
     st.plotly_chart(fig)
+
+    fig = make_subplots(rows=1, cols=1, shared_xaxes=True,)
+    for state in ['H','IC']:
+        fig.add_trace(go.Scatter(x=list(range(steps)), y=results[state], mode='lines', name=state), row=1, col=1)
+        fig.update_layout(title_text="Hospital and IC occupation")
+    st.plotly_chart(fig)
+
+    fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
+    for state in ['S', 'R']:
+        fig.add_trace(go.Scatter(x=list(range(steps)), y=results[state], mode='lines', name=state), row=1, col=1)
+        fig.update_layout(title_text="Suspectible and recovered")
+    st.plotly_chart(fig)
+
+    fig = make_subplots(rows=1, cols=1, shared_xaxes=True)
+    for state in ['D']:
+        fig.add_trace(go.Scatter(x=list(range(steps)), y=results[state], mode='lines', name=state), row=1, col=1)
+        fig.update_layout(title_text="Deaths")
+    st.plotly_chart(fig)
+
+   
 
  
 if __name__ == "__main__":
