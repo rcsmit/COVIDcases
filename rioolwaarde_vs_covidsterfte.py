968ff872734fa9ea521b13a8c8050811b20a0e82
Rene Smit <rcx.smit@gmail.com>
2025-03-06 15:25:48 +0700

    .

--- rioolwaarde_vs_covidsterfte.py
+++ rioolwaarde_vs_covidsterfte.py
@@ -2,6 +2,8 @@ import pandas as pd
 from datetime import datetime, timedelta
 import streamlit as st
 import plotly.graph_objects as go
+import numpy as np
+import plotly.express as px
 
 # Define a function to calculate the month from the selected year and week
 def calculate_month(row):
@@ -10,8 +12,8 @@ def calculate_month(row):
     return first_day_of_week.month
 
 def calculate_month_oversterfte(row):
-    first_day_of_year = datetime(row['jaar_z'], 1, 1)
-    first_day_of_week = first_day_of_year + timedelta(weeks=row['week_z'] - 1)
+    first_day_of_year = datetime(int(row['jaar_x_x']), 1, 1)
+    first_day_of_week = first_day_of_year + timedelta(weeks=int(row['week_x_x']) - 1)
     return first_day_of_week.month
 
 # Define a function to select the appropriate year based on the week
@@ -21,17 +23,112 @@ def select_year(row):
     else:
         return int(row['year'].split('/')[1])
 
+
+def make_lineplot(merged_df):
+    # Create a line plot with two axes using Plotly
+    fig = go.Figure()
+
+    # Add the rioolwater data to the plot
+    fig.add_trace(go.Scatter(x=merged_df['selected_year'].astype(str) + '-' + merged_df['month'].astype(str),
+                            y=merged_df['value_rioolwater'],
+                            mode='lines',
+                            name='Rioolwater',
+                            yaxis='y1'))
+
+    # Add the covid sterfte data to the plot
+    fig.add_trace(go.Scatter(x=merged_df['selected_year'].astype(str) + '-' + merged_df['month'].astype(str),
+                            y=merged_df['value_covid_sterfte'],
+                            mode='lines',
+                            name='Covid Sterfte',
+                            yaxis='y1'))
+
+    # Add the oversterfte data to the plot
+    fig.add_trace(go.Scatter(x=merged_df['selected_year'].astype(str) + '-' + merged_df['month'].astype(str),
+                            y=merged_df['oversterfte_cbs'],
+                            mode='lines',
+                            name='Oversterfte',
+                            #  yaxis='y3'
+                            ))
+
+    # Update the layout to include three y-axes
+    fig.update_layout(
+        title='Rioolwater vs Covid Sterfte vs Oversterfte',
+        xaxis_title='Year-Month',
+        yaxis=dict(
+            title='Rioolwater Value',
+            titlefont=dict(
+                color='#1f77b4'
+            ),
+            tickfont=dict(
+                color='#1f77b4'
+            )
+        ),
+    
+    )
+
+    # Display the plot
+    st.plotly_chart(fig)
+
+
+
+def make_scatterplot(df_temp, what_to_show_l, what_to_show_r,):
+    
+    what_to_show_l = what_to_show_l if type(what_to_show_l) == list else [what_to_show_l]
+    what_to_show_r = what_to_show_r if type(what_to_show_r) == list else [what_to_show_r]
+    x_ = np.array(df_temp[what_to_show_l])
+    y_ = np.array(df_temp[what_to_show_r])
+            
+
+    idx = np.isfinite(x_) & np.isfinite(y_)
+    m, b = np.polyfit(x_[idx], y_[idx], 1)
+    model = np.polyfit(x_[idx], y_[idx], 1)
+
+    predict = np.poly1d(model)
+    from sklearn.metrics import r2_score
+
+    r2 = r2_score  (y_[idx], predict(x_[idx]))
+
+    fig1xyz = px.scatter(df_temp, x=what_to_show_l[0], y=what_to_show_r[0], 
+                        trendline="ols", trendline_scope = 'overall',trendline_color_override = 'black'
+            )
+
+    correlation_sp = round(df_temp[what_to_show_l[0]].corr(df_temp[what_to_show_r[0]], method='spearman'), 3) #gebruikt door HJ Westeneng, rangcorrelatie
+    correlation_p = round(df_temp[what_to_show_l[0]].corr(df_temp[what_to_show_r[0]], method='pearson'), 3)
+
+    title_scatter_plotly = (f"{what_to_show_l[0]} -  {what_to_show_r[0]}<br>Correlation spearman = {correlation_sp} - Correlation pearson = {correlation_p}<br>y = {round(m,2)}*x + {round(b,2)} | r2 = {round(r2,4)}")
+
+    fig1xyz.update_layout(
+        title=dict(
+            text=title_scatter_plotly,
+            x=0.5,
+            y=0.95,
+            font=dict(
+                family="Arial",
+                size=14,
+                color='#000000'
+            )
+        ),
+        xaxis_title=what_to_show_l[0],
+        yaxis_title=what_to_show_r[0],
+
+    )
+
+
+    st.plotly_chart(fig1xyz, use_container_width=True)
+
 # Load the CSV files
 rioolwater_url = "https://raw.githubusercontent.com/rcsmit/COVIDcases/main/input/rioolwater_2025mrt.csv"
 covid_sterfte_url = "https://raw.githubusercontent.com/rcsmit/COVIDcases/main/input/overlijdens_covid_months_as_int.csv"
-oversterfte_url =  "https://raw.githubusercontent.com/rcsmit/COVIDcases/main/input/overl_cbs_vs_rivm.csv"
+# oversterfte_url =  "https://raw.githubusercontent.com/rcsmit/COVIDcases/main/input/overl_cbs_vs_rivm.csv"
 oversterfte_url = "https://raw.githubusercontent.com/rcsmit/COVIDcases/main/input/oversterfte_mrt2025.csv"
+#oversterfte_url = r"C:\Users\rcxsm\Documents\python_scripts\covid19_seir_models\COVIDcases\input\oversterfte_mrt2025.csv"
 # https://www.rivm.nl/corona/actueel/weekcijfers
 # https://www.cbs.nl/nl-nl/reeksen/tijd/doodsoorzaken
 
 rioolwater = pd.read_csv(rioolwater_url, delimiter=';')
 covid_sterfte = pd.read_csv(covid_sterfte_url, delimiter=',')
 oversterfte = pd.read_csv(oversterfte_url, delimiter=',')
+
 
 # Process oversterfte data
 oversterfte['month'] = oversterfte.apply(calculate_month_oversterfte, axis=1)
@@ -55,46 +152,6 @@ final_df_covid_sterfte['year'] = final_df_covid_sterfte['year'].astype(int)
 merged_df = pd.merge(grouped_df_rioolwater, final_df_covid_sterfte, left_on=['selected_year', 'month'], right_on=['year', 'month'], suffixes=('_rioolwater', '_covid_sterfte'))
 merged_df = pd.merge(merged_df, grouped_oversterfte, left_on=['selected_year', 'month'], right_on=['jaar_z', 'month'])
 
-# Create a line plot with two axes using Plotly
-fig = go.Figure()
-
-# Add the rioolwater data to the plot
-fig.add_trace(go.Scatter(x=merged_df['selected_year'].astype(str) + '-' + merged_df['month'].astype(str),
-                         y=merged_df['value_rioolwater'],
-                         mode='lines',
-                         name='Rioolwater',
-                         yaxis='y1'))
-
-# Add the covid sterfte data to the plot
-fig.add_trace(go.Scatter(x=merged_df['selected_year'].astype(str) + '-' + merged_df['month'].astype(str),
-                         y=merged_df['value_covid_sterfte'],
-                         mode='lines',
-                         name='Covid Sterfte',
-                         yaxis='y1'))
-
-# Add the oversterfte data to the plot
-fig.add_trace(go.Scatter(x=merged_df['selected_year'].astype(str) + '-' + merged_df['month'].astype(str),
-                         y=merged_df['oversterfte_cbs'],
-                         mode='lines',
-                         name='Oversterfte',
-                        #  yaxis='y3'
-                        ))
-
-# Update the layout to include three y-axes
-fig.update_layout(
-    title='Rioolwater vs Covid Sterfte vs Oversterfte',
-    xaxis_title='Year-Month',
-    yaxis=dict(
-        title='Rioolwater Value',
-        titlefont=dict(
-            color='#1f77b4'
-        ),
-        tickfont=dict(
-            color='#1f77b4'
-        )
-    ),
-   
-)
-
-# Display the plot
-st.plotly_chart(fig)
+make_lineplot(merged_df)
+make_scatterplot(merged_df, "value_rioolwater", "value_covid_sterfte")
+make_scatterplot(merged_df, "value_rioolwater", "oversterfte_cbs")
